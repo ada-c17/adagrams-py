142f8fa534e08db4423ef5bb91bcbc61a2cf2e9b
Audrey Andoy <audreylandoy@gmail.com>
2021-09-27 12:47:54 -0700

    winning word is not calculating highest score

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,65 +1,79 @@
 import random
 
 def draw_letters():
-    distribution = {'A': 9, 'B': 2, 'O': 8, 'C': 2, 'P': 2, 
+    letters = {'A': 9, 'B': 2, 'O': 8, 'C': 2, 'P': 2, 
                     'D': 4, 'Q': 1, 'E': 12, 'R': 6, 'F': 2, 
                     'S': 4, 'G': 3, 'T': 6, 'H': 2, 'U': 4, 
                     'I': 9, 'V': 2, 'J': 1, 'W': 2, 'K': 1, 
                     'X': 1, 'L': 4, 'Y': 2, 'M': 2, 'Z': 1}
-    pool = []
-    for ltr in distribution:
-        pool.extend([ltr for i in range(distribution[ltr])])
-    # Using random.sample:
-    #hand = random.sample(pool, 10)
-    # manual version:
     hand = []
-    for i in range(10):
-        # randint is inclusive
-        rand = random.randint(0, len(pool) - 1)
-        ltr = pool[rand]
-        hand.append(ltr)
-        pool.remove(ltr)
 
+    while len(hand) < 10:
+        letter = random.choice(list(letters))
+        if letters[letter] > 0:
+            hand.append(letter)
+            letters[letter] -= 1
     return hand
 
+
 def uses_available_letters(word, letter_bank):
-    word = word.upper()
-    ltr_dict = {}
-    for ltr in letter_bank:
-        if ltr in ltr_dict:
-            ltr_dict[ltr] += 1
+    is_anagram = True
+    letters = list(letter_bank)
+    
+    for letter in word:
+        if letter in letters:
+            letters.remove(letter)
         else:
-            ltr_dict[ltr] = 1
-    for ltr in word:
-        if ltr in ltr_dict and ltr_dict[ltr] > 0:
-            ltr_dict[ltr] -= 1
-        else:
-            return False
-    return True
+            is_anagram = False
+
+    return is_anagram
 
 def score_word(word):
-    scores = {'A': 1, 'B': 3, 'O': 1, 'C': 3, 'P': 3, 
-                'D': 2, 'Q': 10, 'E': 1, 'R': 1, 'F': 4, 
-                'S': 1, 'G': 2, 'T': 1, 'H': 4, 'U': 1, 
-                'I': 1, 'V': 4, 'J': 8, 'W': 4, 'K': 5, 
-                'X': 8, 'L': 1, 'Y': 4, 'M': 3, 'Z': 10}
-    word = word.upper()
     score = 0
-    for ltr in word:
-        score += scores[ltr]
-    if len(word) >= 7:
+    if len(word) >= 7 and len(word) <= 10:
         score += 8
-    return score
+
+    word = word.upper()
+
+    for letter in word:
+        if letter in "AEIOULNRST":
+            score += 1
+        elif letter in "DG":
+            score += 2
+        elif letter in "BCMP":
+            score += 3
+        elif letter in "FHVWY":
+            score += 4
+        elif letter == "K":
+            score += 5
+        elif letter in "JX":
+            score += 8
+        elif letter in "QZ":
+            score += 10
+
+    return score 
+
+    
 
 def get_highest_word_score(word_list):
-    highest_word = ""
-    highest_score = 0
+    highest_score = 0 
+    highest_scoring_word = ""
+    prev_word = ""
+
     for word in word_list:
-        score = score_word(word)
-        if score > highest_score or \
-           (score == highest_score and len(word) > len(highest_word) and len(word) == 10) or \
-           score == highest_score and len(highest_word) != 10 and len(word) < len(highest_word):
-            highest_word = word
-            highest_score = score
-        
-    return highest_word, highest_score
+        word_score = score_word(word)
+
+        if word_score > highest_score:
+                highest_score = word_score
+                highest_scoring_word = word
+        elif word_score == highest_score:
+            if len(word) < len(highest_scoring_word) and len(highest_scoring_word) != 10:
+                highest_score = word_score
+                highest_scoring_word = word
+            elif len(word) == len(highest_scoring_word) and word_score == highest_score:
+                break
+            elif len(word) == 10 and word_score == highest_score:
+                highest_score = word_score
+                highest_scoring_word = word
+
+    return (highest_scoring_word, highest_score)
