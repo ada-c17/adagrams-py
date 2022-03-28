749fad9b8de2c4f79dc472ea764521908d72b556
Tiffini Hyatt <tiffini.hyatt@gmail.com>
2022-03-28 14:00:52 -0500

    Started draw_letters function for wave 01

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,5 +1,21 @@
+from random import choice, randint
+from copy import deepcopy
+
+# create a list holding all 98 letters (letter_bank)
+
 def draw_letters():
     pass
+    user_hand = []
+    copy_of_letter_bank = deepcopy(letter_bank)
+
+    for i in range(10):
+        index = randint(0, len(copy_of_letter_bank) - 1)
+        letter = copy_of_letter_bank.pop(index)
+        user_hand.append(letter)
+
+    # for i in range(11):
+    #     if 
+    #     user_hand.append(choice(letter_pool))
 
 def uses_available_letters(word, letter_bank):
     pass
