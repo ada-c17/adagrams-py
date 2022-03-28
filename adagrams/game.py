176de2357fa1824ff46f48e119335c7a80f2c0c6
Chinazo Onwukaike <chinazoonwukaike@gmail.com>
2022-03-28 18:29:36 -0500

    complete wave 03

--- adagrams/game.py
+++ adagrams/game.py
@@ -69,7 +69,24 @@ def uses_available_letters(word, letter_bank):
     return True
 
 def score_word(word):
-    pass
+    score = 0
+    points_dict = {
+        1 : ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"],
+        2 : ["D", "G"],
+        3 : ["B", "C", "M", "P"],
+        4 : ["F", "H", "V", "W", "Y"],
+        5 : ["K"],
+        8 : ["J", "X"],
+        10 : ["Q", "Z"]
+    } 
+
+    for letter in word.upper():
+        for key in points_dict:
+            if letter in points_dict[key]:
+                score += key
+    if len(word) >= 7 and len(word) <= 10:
+        score += 8
+    return score 
 
 def get_highest_word_score(word_list):
     pass
