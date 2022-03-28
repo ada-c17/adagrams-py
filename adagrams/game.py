d11b28181983f7604ee299a219b5e5ce22351ca4
Jamie H <jamiewhorvath@gmail.com>
2022-03-28 15:01:18 -0400

    psudocode for all functions in game.py

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,11 +1,38 @@
 def draw_letters():
+    #define list of available letters
+    #take 10 random rolls each with one less number
+    #pop each out between rolls and store in list
+    #return list of rolled letters
     pass
 
 def uses_available_letters(word, letter_bank):
+    #make input uppercase
+    #make copy of list of letters
+    #loop through the word index string
+    #loop through letter list and pop found letter
+    #if letter not found return false
+    #return true
     pass
 
+
 def score_word(word):
+    #test for zero length
+    #define list of dictionaries for points
+    #upper() the word
+    #len()>6 for 8 additional points
+    #loop through word
+    #  lookup point value in dictionary
+    #  add to sum
+    #return sum
     pass
 
 def get_highest_word_score(word_list):
+    #test for zero length
+    #define word,score tuple
+    #loop through passed list
+    #  use wave 3 to calculate score
+    #  compare score
+    #  compare length is same
+    #    replace tuple with index word and score
+    #return tuple
     pass
