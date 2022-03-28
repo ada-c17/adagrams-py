7621fef303c42dbd06ab77528f321617ff46d523
Philomena Kelly <philomena.a.kelly@outlook.com>
2022-03-28 14:54:27 -0400

    Passed wave 3 tests

--- adagrams/game.py
+++ adagrams/game.py
@@ -2,32 +2,32 @@ import random
 from collections import Counter
 
 LETTER_POOL = {
-    'A': 9, 
-    'B': 2, 
-    'C': 2, 
-    'D': 4, 
-    'E': 12, 
-    'F': 2, 
-    'G': 3, 
-    'H': 2, 
-    'I': 9, 
-    'J': 1, 
-    'K': 1, 
-    'L': 4, 
-    'M': 2, 
-    'N': 6, 
-    'O': 8, 
-    'P': 2, 
-    'Q': 1, 
-    'R': 6, 
-    'S': 4, 
-    'T': 6, 
-    'U': 4, 
-    'V': 2, 
-    'W': 2, 
-    'X': 1, 
-    'Y': 2, 
-    'Z': 1
+    'A': [9, 1], 
+    'B': [2, 3], 
+    'C': [2, 3], 
+    'D': [4, 2], 
+    'E': [12, 1], 
+    'F': [2, 4], 
+    'G': [3, 2], 
+    'H': [2, 4], 
+    'I': [9, 1],
+    'J': [1, 8],
+    'K': [1, 5],
+    'L': [4, 1],
+    'M': [2, 3],
+    'N': [6, 1], 
+    'O': [8, 1],
+    'P': [2, 3],
+    'Q': [1, 10], 
+    'R': [6, 1],
+    'S': [4, 1],
+    'T': [6, 1], 
+    'U': [4, 1], 
+    'V': [2, 4], 
+    'W': [2, 4], 
+    'X': [1, 8], 
+    'Y': [2, 4],
+    'Z': [1, 10]
 }
 
 
@@ -36,7 +36,7 @@ def draw_letters():
     letter_list = []
     returned_letters = []
     for letter, number in LETTER_POOL.items():
-        letter_list += letter * number
+        letter_list += letter * number[0]
     for i in range(10):
         current_letter = letter_list[random.randint(0, len(letter_list) - 1)]
         returned_letters.append(current_letter)
@@ -60,7 +60,13 @@ def uses_available_letters(word, letter_bank):
     return True
 
 def score_word(word):
-    pass
+    score = 0
+    for letter in word:
+        score += LETTER_POOL[letter.upper()][1]
+    if len(word) > 6:
+        score += 8
+    return score
+
 
 def get_highest_word_score(word_list):
     pass
