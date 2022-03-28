8178137ade132989ae89156f37ab673569c869aa
Gabriela Ibarra <gabriela.estefania.ibarra@gmail.com>
2022-03-28 14:03:39 -0500

    Updated draw_letters function

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,6 +1,48 @@
 def draw_letters():
     pass
 
+    LETTER_POOL = {
+        'A': 9, 
+        'B': 2, 
+        'C': 2, 
+        'D': 4, 
+        'E': 12, 
+        'F': 2, 
+        'G': 3, 
+        'H': 2, 
+        'I': 9, 
+        'J': 1, 
+        'K': 1, 
+        'L': 4, 
+        'M': 2, 
+        'N': 6, 
+        'O': 8, 
+        'P': 2, 
+        'Q': 1, 
+        'R': 6, 
+        'S': 4, 
+        'T': 6, 
+        'U': 4, 
+        'V': 2, 
+        'W': 2, 
+        'X': 1, 
+        'Y': 2, 
+        'Z': 1
+    }
+
+    hand = []
+    for i in range(10):
+        pass
+    print(hand)
+    
+    # Empty list = hand
+    # For loop to iterate over letter pool
+        # Iterate 10 times
+        # Append letter to list (hand)
+        # Append letter and count to dictionary 
+        # E.g. "a", "b", "e", "f", "a"
+        # a: 2, b: 1       
+
 def uses_available_letters(word, letter_bank):
     pass
 
