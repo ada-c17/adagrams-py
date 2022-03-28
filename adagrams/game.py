231c2e9219c0e7067ec171ec46593dda75c65035
Morgan Adkisson <adkissonmorgan@gmail.com>
2022-03-28 16:01:47 -0700

    group additions: dictionary of all_available_letters and returned pool_of_letters

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,5 +1,40 @@
+import random 
+
 def draw_letters():
-    pass
+    # Returns an array of ten strings - letters should be randomly drawn from a pool of letters
+    # Invoking this function should not change the pool of letters
+
+    all_available_letters = {
+            'A': 9, 
+            'B': 2, 
+            'C': 2, 
+            'D': 4, 
+            'E': 12, 
+            'F': 2, 
+            'G': 3, 
+            'H': 2, 
+            'I': 9, 
+            'J': 1, 
+            'K': 1, 
+            'L': 4, 
+            'M': 2, 
+            'N': 6, 
+            'O': 8, 
+            'P': 2, 
+            'Q': 1, 
+            'R': 6, 
+            'S': 4, 
+            'T': 6, 
+            'U': 4, 
+            'V': 2, 
+            'W': 2, 
+            'X': 1, 
+            'Y': 2, 
+            'Z': 1
+        }
+
+    pool_of_letters = random.choices(list(all_available_letters.keys()), weights=all_available_letters.values(), k = 10)
+    return pool_of_letters
 
 def uses_available_letters(word, letter_bank):
     pass
