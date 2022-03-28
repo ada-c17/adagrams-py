7727f54c2007a8f0cde66a6cc753cdfee97b61db
Emily Schell <lauraemilyschell@gmail.com>
2022-03-28 19:45:05 -0400

    complete uses_available_letters function to pass wave 2 tests

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,11 +1,24 @@
 def draw_letters():
     pass
 
+
 def uses_available_letters(word, letter_bank):
-    pass
+    letters_valid = True
+    letter_bank_copy = []
+    for letter in letter_bank:
+        letter_bank_copy.append(letter.lower())
+    for letter in word:
+        letter = letter.lower()
+        if letter in letter_bank_copy:
+            letter_bank_copy.remove(letter)
+        else:
+            letters_valid = False
+    return letters_valid
+
 
 def score_word(word):
     pass
+
 
 def get_highest_word_score(word_list):
     pass
