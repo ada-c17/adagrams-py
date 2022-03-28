0f61e88d7f5d9fb7272b07cab360b0927140471f
Sreymompech <sanchhen2013@gmail.com>
2022-03-28 16:25:25 -0700

    Coding on Wave 1: draw_letters and Wave 2: use_available_letters

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,8 +1,54 @@
+import string
+import random
+
+
+LETTER_POOL = {
+    'A': 9, 
+    'B': 2, 
+    'C': 2, 
+    'D': 4, 
+    'E': 12, 
+    'F': 2, 
+    'G': 3, 
+    'H': 2, 
+    'I': 9, 
+    'J': 1, 
+    'K': 1, 
+    'L': 4, 
+    'M': 2, 
+    'N': 6, 
+    'O': 8, 
+    'P': 2, 
+    'Q': 1, 
+    'R': 6, 
+    'S': 4, 
+    'T': 6, 
+    'U': 4, 
+    'V': 2, 
+    'W': 2, 
+    'X': 1, 
+    'Y': 2, 
+    'Z': 1
+}
+
 def draw_letters():
-    pass
+    list_letter = []
+    for letter, count in LETTER_POOL.items():
+        for i in range(0, count):
+            list_letter.append(letter)
+    letter_draw = random.sample(list_letter, k=10)
+    
+    return letter_draw
+
 
 def uses_available_letters(word, letter_bank):
-    pass
+    letter_bank_copy = letter_bank.copy()
+    for char in word.upper():
+        if char not in letter_bank_copy:
+            return False
+        else:
+            letter_bank_copy.remove(char)
+    return True
 
 def score_word(word):
     pass
