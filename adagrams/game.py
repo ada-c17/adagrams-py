157021324fc1214f887d144e6b701903ec28740b
Ying G <yingg@yings-air.lan>
2022-03-28 16:23:35 -0700

    pass all wave 1 tests

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,5 +1,45 @@
+import random
+
+LETTER_POOL = {
+    'A': 9, 
+    'B': 2, 
+    'C': 2, 
+    'D': 4, 
+    'E': 12, 
+    'F': 2, 
+    'G': 3, 
+    'H': 2, 
+    'I': 9, 
+    'J': 1, 
+    'K': 1, 
+    'L': 4, 
+    'M': 2, 
+    'N': 6, 
+    'O': 8, 
+    'P': 2, 
+    'Q': 1, 
+    'R': 6, 
+    'S': 4, 
+    'T': 6, 
+    'U': 4, 
+    'V': 2, 
+    'W': 2, 
+    'X': 1, 
+    'Y': 2, 
+    'Z': 1
+}
+
+
 def draw_letters():
-    pass
+    # copy a dictionary so that we don't change the data, it's constant
+    LETTER_POOL_COPY = LETTER_POOL.copy()
+    letters_drawn = []
+    while len(letters_drawn) < 10:
+        letter_drawn = random.choice(list(LETTER_POOL_COPY)) 
+        if LETTER_POOL_COPY[letter_drawn] >= 1: 
+            letters_drawn.append(letter_drawn) 
+            LETTER_POOL_COPY[letter_drawn] -= 1 
+    return letters_drawn
 
 def uses_available_letters(word, letter_bank):
     pass
