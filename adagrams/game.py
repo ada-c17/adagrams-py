7e553d1d413f5e4dcf09bcfbcf825890f6f7d0ef
Thu Vuong <tavuongg@gmail.com>
2022-03-28 14:02:00 -0700

    added helper function to create letter pool and provided suggestion for draw_letters function

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,10 +1,27 @@
 from random import choice, randint
 from copy import deepcopy
+from data import LETTER_POOL
 
 # create a list holding all 98 letters (letter_bank)
+def create_letter_pool():
+    letter_pool = []
+    for letter, frequency in LETTER_POOL.items():
+        for i in range(frequency):
+            letter_pool.append(letter)
+    
+    return letter_pool
+
+# thu's suggestion for improving draw_letters():
+# def draw_letters():
+
+#     # it will restart every time this function is invoked
+#     letter_pool = create_letter_pool()
+#     shuffle(letter_pool)
+#     user_hand = letter_pool[0:10]
+
+#     return user_hand
 
 def draw_letters():
-    pass
     user_hand = []
     copy_of_letter_bank = deepcopy(letter_bank)
 
