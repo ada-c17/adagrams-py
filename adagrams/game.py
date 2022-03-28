77a7c09761b4467822c61aea694dac0af328ca0d
Jodi Denney <iarrived@gmail.com>
2022-03-28 13:08:40 -0700

    Finished draw_letter function - return list of 10 random letters

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,11 +1,59 @@
+from random import random
+
+
+import random
+
+
 def draw_letters():
-    pass
+    # Return: ["A", "C", "D", "W"]
+    # Import Random
+    # Import dictionary from Test
+    # Write a function  to Create comprehensive list of all letter and their frequency from dictionary
+    # Random Shuffle List
+    # Return First Ten
+    LETTER_POOL = {
+        'A': 9,
+        'B': 2,
+        'C': 2,
+        'D': 4,
+        'E': 12,
+        'F': 2,
+        'G': 3,
+        'H': 2,
+        'I': 9,
+        'J': 1,
+        'K': 1,
+        'L': 4,
+        'M': 2,
+        'N': 6,
+        'O': 8,
+        'P': 2,
+        'Q': 1,
+        'R': 6,
+        'S': 4,
+        'T': 6,
+        'U': 4,
+        'V': 2,
+        'W': 2,
+        'X': 1,
+        'Y': 2,
+        'Z': 1
+    }
+    players_list = []
+
+    for letter in LETTER_POOL:
+        players_list.extend([letter] * LETTER_POOL[letter])
+    random.shuffle(players_list)
+    return players_list[:10]
+
 
 def uses_available_letters(word, letter_bank):
     pass
 
+
 def score_word(word):
     pass
+
 
 def get_highest_word_score(word_list):
     pass
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 from adagrams.ui_helper import *
 from adagrams.game import draw_letters, uses_available_letters, score_word, get_highest_word_score
 
+
 def wave_1_run_game():
     display_welcome_message()
     game_continue = True
@@ -9,12 +10,13 @@ def wave_1_run_game():
         print("Let's draw 10 letters from the letter pool...")
         letter_bank = draw_letters()
         display_drawn_letters(letter_bank)
-    
+
         display_retry_instructions()
         continue_input = input()
         game_continue = continue_input == "y"
-   
+
     display_goodbye_message()
+
 
 def wave_2_run_game():
     display_welcome_message()
@@ -26,15 +28,16 @@ def wave_2_run_game():
         display_game_instructions()
         user_input_word = input()
 
-        while( not uses_available_letters(user_input_word, letter_bank)):
+        while(not uses_available_letters(user_input_word, letter_bank)):
             display_needs_valid_input_message()
             user_input_word = input()
 
         display_retry_instructions()
         continue_input = input()
         game_continue = continue_input == "y"
-   
+
     display_goodbye_message()
+
 
 def wave_3_run_game():
     display_welcome_message()
@@ -46,10 +49,10 @@ def wave_3_run_game():
         display_game_instructions()
         user_input_word = input()
 
-        while( not uses_available_letters(user_input_word, letter_bank)):
+        while(not uses_available_letters(user_input_word, letter_bank)):
             display_needs_valid_input_message()
             user_input_word = input()
-        
+
         score = score_word(user_input_word)
         display_score(score)
 
@@ -57,6 +60,7 @@ def wave_3_run_game():
         continue_input = input()
         game_continue = continue_input == "y"
     display_goodbye_message()
+
 
 def wave_4_run_game():
     display_welcome_message()
@@ -69,10 +73,10 @@ def wave_4_run_game():
         display_game_instructions()
         user_input_word = input()
 
-        while( not uses_available_letters(user_input_word, letter_bank)):
+        while(not uses_available_letters(user_input_word, letter_bank)):
             display_needs_valid_input_message()
             user_input_word = input()
-        
+
         score = score_word(user_input_word)
         display_score(score)
         played_words.append(user_input_word)
@@ -82,6 +86,7 @@ def wave_4_run_game():
         game_continue = continue_input == "y"
     display_highest_score(get_highest_word_score(played_words))
     display_goodbye_message()
+
 
 def main(wave):
     if(wave == 1):
@@ -94,6 +99,7 @@ def main(wave):
         wave_4_run_game()
     else:
         print("Please input a wave number.  Valid wave numbers are 1, 2, 3, 4.")
+
 
 if __name__ == "__main__":
     args = sys.argv
