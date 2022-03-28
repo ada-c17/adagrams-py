4974ba0e5a5c22a32921b58e275fe14b33132498
K <karinrfrank@gmail.com>
2022-03-28 12:01:21 -0700

    Wave 1 pseudocode

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,5 +1,10 @@
 def draw_letters():
-    pass
+    # import random
+    # letters = [list of letters]
+    # loop through key, value in dictionary of letters
+    # append key to the list, repeat this value number of times
+    # use random.sample(letters, 10) to get 10 non-repeating letters from the list
+
 
 def uses_available_letters(word, letter_bank):
     pass
