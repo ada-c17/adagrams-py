923ad81a5853f805a034360615e30fe0e9459c13
Liliana <liliana.m.parra@gmail.com>
2022-03-28 14:58:42 -0400

    Made function draw_letters return a list of 10 letters. Need to add while loop.

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,5 +1,42 @@
+import random 
+available_letters = {
+    'A': 9, 
+    'B': 2, 
+    'C': 2, 
+    'D': 4, 
+    'E': 12, 
+    'F': 2, 
+    'G': 3, 
+    'H': 2, 
+    'I': 9, 
+    'J': 1, 
+    'K': 1, 
+    'L': 4, 
+    'M': 2, 
+    'N': 6, 
+    'O': 8, 
+    'P': 2, 
+    'Q': 1, 
+    'R': 6, 
+    'S': 4, 
+    'T': 6, 
+    'U': 4, 
+    'V': 2, 
+    'W': 2, 
+    'X': 1, 
+    'Y': 2, 
+    'Z': 1
+}
 def draw_letters():
-    pass
+    letter_hand = []
+    for i in range (10):
+        picked_letter = random.choice(list(available_letters.keys()))
+        # check frequency
+        if letter_hand.count(picked_letter) < available_letters[picked_letter]:
+            letter_hand.append(picked_letter)
+    return letter_hand    
+        
+print(draw_letters())
 
 def uses_available_letters(word, letter_bank):
     pass
