78b1c2ed78796dad2cd335ddfb35f0de24961b6a
jeannie <jeanniezheng123@gmail.com>
2022-03-28 18:32:11 -0500

    function draw_letters complete using string method

--- adagrams/game.py
+++ adagrams/game.py
@@ -47,12 +47,58 @@ def draw_letters():
     # if enough quantity
     # subtract quantity for each letter
         # check to make sure it's > 0
-    COPY_LETTER_POOL = copy.deepcopy(LETTER_POOL)
-    random_letter = random.choice(list(COPY_LETTER_POOL))
-    print("***PRINT TEST***")
-    print(random_letter)
-    
+    # COPY_LETTER_POOL = copy.deepcopy(LETTER_POOL)
+    # random_letter = random.choice(list(COPY_LETTER_POOL))
+    # print("***PRINT TEST***")
+    # print(random_letter)
+    LETTER_POOL = {
+    'A': 9, 
+    'B': 2, 
+    'C': 2, 
+    'D': 4, 
+    'E': 12, 
+    'F': 2, 
+    'G': 3, 
+    'H': 2, 
+    'I': 9, 
+    'J': 1, 
+    'K': 1, 
+    'L': 4, 
+    'M': 2, 
+    'N': 6, 
+    'O': 8, 
+    'P': 2, 
+    'Q': 1, 
+    'R': 6, 
+    'S': 4, 
+    'T': 6, 
+    'U': 4, 
+    'V': 2, 
+    'W': 2, 
+    'X': 1, 
+    'Y': 2, 
+    'Z': 1
+}
+    player_hand = []
+    random_letter = ""
+
+    letter_count = len(player_hand)
+    print(letter_count)
+    while letter_count < 10:
+        string.ascii_letters
+        random_letter = random.choice(string.ascii_uppercase)
+        
+        if LETTER_POOL[random_letter] == 0:
+            continue
+        else:
+            player_hand.append(random_letter)
+            LETTER_POOL[random_letter] -= 1
+            letter_count += 1
+        
+    return player_hand
+
 print(draw_letters())
+
 def uses_available_letters(word, letter_bank):
     pass
 
