5e9f0568f10d1d9dc8d810b8dc64f6f94a99ac68
HaileyMatz <hmatz525@gmail.com>
2022-03-28 19:17:04 -0700

    completed draw_letters function

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,5 +1,45 @@
+import copy 
+import random
+
+
+LETTER_POOL = {
+    'A': 9, 
+    'B': 2, 
+    'C': 2, 
+    'D': 4, 
+    'E': 12, 
+    'F': 2, 
+    'G': 3, 
+    'H': 2, 
+    'I': 9, 
+    'J': 1, 
+    'K': 1, 
+    'L': 4, 
+    'M': 2, 
+    'N': 6, 
+    'O': 8, 
+    'P': 2, 
+    'Q': 1, 
+    'R': 6, 
+    'S': 4, 
+    'T': 6, 
+    'U': 4, 
+    'V': 2, 
+    'W': 2, 
+    'X': 1, 
+    'Y': 2, 
+    'Z': 1
+}
+
 def draw_letters():
-    pass
+    ten_letters = []
+    no_change_letters = LETTER_POOL.copy()
+    for i in range(10):
+        random_letter = random.choice(list(LETTER_POOL))
+        if no_change_letters[random_letter] > 0:
+            ten_letters.append(random_letter)
+            no_change_letters[random_letter] -= 1
+    return ten_letters
 
 def uses_available_letters(word, letter_bank):
     pass
@@ -9,3 +49,4 @@ def score_word(word):
 
 def get_highest_word_score(word_list):
     pass
+
