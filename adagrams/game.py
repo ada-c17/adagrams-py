7b59572de741522beb1bf98d97ffe70df948d127
Jamie H <jamiewhorvath@gmail.com>
2022-03-28 22:30:55 -0400

    Completed wave 3 and clarified tests for wave 3

--- adagrams/game.py
+++ adagrams/game.py
@@ -44,6 +44,22 @@ def score_word(word):
     #  lookup point value in dictionary
     #  add to sum
     #return sum
+    # if len(word) == 0:
+    #     return None
+    score_dict = {  'A' : 1, 'E' : 1, 'I' : 1, 'O' : 1, 'U' : 1, 'L' : 1, 'N' : 1, 'R' : 1, 'S' : 1, 'T' : 1,
+                    'D' : 2, 'G' : 2,
+                    'B' : 3, 'C' : 3, 'M' : 3, 'P' : 3,
+                    'F' : 4, 'H' : 4, 'V' : 4, 'W' : 4, 'Y' : 4,
+                    'K' : 5,
+                    'J' : 8, 'X' : 8,
+                    'Q' : 10, 'Z' : 10}
+    word = word.upper()
+    score = 0
+    if len(word) > 6:
+        score += 8
+    for letter in word:
+        score += score_dict[letter]
+    return score
     pass
 
 def get_highest_word_score(word_list):
--- tests/test_wave_03.py
+++ tests/test_wave_03.py
@@ -1,26 +1,66 @@
 import pytest
 
 from adagrams.game import score_word
+score_dict = {  'A' : 1, 'E' : 1, 'I' : 1, 'O' : 1, 'U' : 1, 'L' : 1, 'N' : 1, 'R' : 1, 'S' : 1, 'T' : 1,
+                'D' : 2, 'G' : 2,
+                'B' : 3, 'C' : 3, 'M' : 3, 'P' : 3,
+                'F' : 4, 'H' : 4, 'V' : 4, 'W' : 4, 'Y' : 4,
+                'K' : 5,
+                'J' : 8, 'X' : 8,
+                'Q' : 10, 'Z' : 10}
 
 def test_score_word_accurate():
+    #Arrange
+    WORD_1 = "A"
+    WORD_2 = "DOG"
+    WORD_3 = "WHIMSY"
+    #Act
+    result_1 = score_word(WORD_1)
+    result_2 = score_word(WORD_2)
+    result_3 = score_word(WORD_3)
+
     # Assert
-    assert score_word("A") == 1
-    assert score_word("DOG") == 5
-    assert score_word("WHIMSY") == 17
+    assert result_1 == 1
+    assert result_2 == 5
+    assert result_3 == 17
 
 def test_score_word_accurate_ignores_case():
+    # Arrange
+    WORD_1 = "a"
+    WORD_2 = "dog"
+    WORD_3 = "wHiMsY"
+        #Act
+    result_1 = score_word(WORD_1)
+    result_2 = score_word(WORD_2)
+    result_3 = score_word(WORD_3)
+
     # Assert
-    assert score_word("a") == 1
-    assert score_word("dog") == 5
-    assert score_word("wHiMsY") == 17
+    assert result_1 == 1
+    assert result_2 == 5
+    assert result_3 == 17
 
 def test_score_zero_for_empty():
+    #Arrange
+    EMPTY_WORD = ""
+    #Act
+    result = score_word(EMPTY_WORD)
+
     # Assert
-    assert score_word("") == 0
+    assert result == 0
 
 def test_score_extra_points_for_seven_or_longer():
+    #Arrange
+    BIG_WORD_1 = "XXXXXXX"
+    BIG_WORD_2 = "XXXXXXXX"
+    BIG_WORD_3 = "XXXXXXXXX"
+
+    #Act
+    result_1 = score_word(BIG_WORD_1)
+    result_2 = score_word(BIG_WORD_2)
+    result_3 = score_word(BIG_WORD_3)
+
     # Assert
-    assert score_word("XXXXXXX") == 64
-    assert score_word("XXXXXXXX") == 72
-    assert score_word("XXXXXXXXX") == 80
+    assert result_1 == 64
+    assert result_2 == 72
+    assert result_3 == 80
     
