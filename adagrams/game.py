40a57b53f2b28ff5455be2c7c013926c3f2d40be
sana <sanakakadan22@yahoo.com>
2022-03-30 01:02:54 +0400

    added wave 3 function, passing 12 tests

--- adagrams/game.py
+++ adagrams/game.py
@@ -29,6 +29,35 @@ LETTER_POOL = {
     'Z': 1
 }
 
+score_chart = {
+    'A': 1, 
+    'B': 3, 
+    'C': 3, 
+    'D': 2, 
+    'E': 1, 
+    'F': 4, 
+    'G': 2, 
+    'H': 4, 
+    'I': 1, 
+    'J': 8, 
+    'K': 5, 
+    'L': 1, 
+    'M': 3, 
+    'N': 1, 
+    'O': 1, 
+    'P': 3, 
+    'Q': 10, 
+    'R': 1, 
+    'S': 1, 
+    'T': 1, 
+    'U': 1, 
+    'V': 4, 
+    'W': 4, 
+    'X': 8, 
+    'Y': 4, 
+    'Z': 10
+}
+
 def draw_letters():
     new_pool = []
     for letter, count in LETTER_POOL.items():
@@ -56,8 +85,14 @@ def uses_available_letters(word, letter_bank):
             return False
     return True
 
+
 def score_word(word):
-    pass
+    total_score = 0
+    for char in word.upper():
+        total_score += score_chart[char]
+    if len(word) > 6:
+        total_score += 8
+    return total_score
 
 def get_highest_word_score(word_list):
     pass
