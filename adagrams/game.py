813c0e6780d5a0cc8f82aa8d5e17d0d65ed58316
Ria Pastor <ria.jacqueline.pastor@gmail.com>
2022-03-29 16:39:20 -0400

    Wave 1 and Wave 2 tests passing

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,28 +1,60 @@
 import random
 
-POOL = ['A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'B', 'B', 'C', 'C',
-        'D', 'D', 'D', 'D', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E',
-        'E', 'E', 'E', 'F', 'F', 'G', 'G', 'G', 'H', 'H', 'I', 'I', 'I',
-        'I', 'I', 'I', 'I', 'I', 'I', 'J', 'K', 'L', 'L', 'L', 'L', 'M',
-        'M', 'N', 'N', 'N', 'N', 'N', 'N', 'O', 'O', 'O', 'O', 'O', 'O',
-        'O', 'O', 'P', 'P', 'Q', 'R', 'R', 'R', 'R', 'R', 'R', 'S', 'S',
-        'S', 'S', 'T', 'T', 'T', 'T', 'T', 'T', 'U', 'U', 'U', 'U', 'V',
-        'V', 'W', 'W', 'X', 'Y', 'Y', 'Z']
+LETTER_POOL = {
+    'A': 9, 
+    'B': 2, 
+    'C': 2, 
+    'D': 4, 
+    'E': 12, 
+    'F': 2, 
+    'G': 3, 
+    'H': 2, 
+    'I': 9, 
+    'J': 1, 
+    'K': 1, 
+    'L': 4, 
+    'M': 2, 
+    'N': 6, 
+    'O': 8, 
+    'P': 2, 
+    'Q': 1, 
+    'R': 6, 
+    'S': 4, 
+    'T': 6, 
+    'U': 4, 
+    'V': 2, 
+    'W': 2, 
+    'X': 1, 
+    'Y': 2, 
+    'Z': 1
+}
 
 def draw_letters():
+    new_pool = []
+    for letter, count in LETTER_POOL.items():
+        new_pool.extend([letter] * count)
+    
     letters = []
     already_drawn = []
     while len(letters) < 10:
-        draw = random.randint(0,97)
+        draw = random.randint(0,len(new_pool)-1)
         if draw not in already_drawn:
-            letters.append(POOL[draw])
+            letters.append(new_pool[draw])
             already_drawn.append(draw)
 
     return letters
 
 
 def uses_available_letters(word, letter_bank):
-    pass
+    uppercase_word = word.upper()
+    bank_copy = list(letter_bank)
+    
+    for char in uppercase_word:
+        if char in bank_copy:
+            bank_copy.remove(char)
+        else:
+            return False
+    return True
 
 def score_word(word):
     pass
