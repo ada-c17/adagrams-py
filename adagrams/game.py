21142ad587f1f20e7b6885f9e3c1fac7031e1877
Jodi Denney <iarrived@gmail.com>
2022-03-29 10:39:29 -0700

    Passed Wave 3.  Created global constants

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,37 +1,50 @@
 import random
 import copy
 
+# dictionary of letters and their frequency
+LETTER_POOL = {
+    'A': 9,
+    'B': 2,
+    'C': 2,
+    'D': 4,
+    'E': 12,
+    'F': 2,
+    'G': 3,
+    'H': 2,
+    'I': 9,
+    'J': 1,
+    'K': 1,
+    'L': 4,
+    'M': 2,
+    'N': 6,
+    'O': 8,
+    'P': 2,
+    'Q': 1,
+    'R': 6,
+    'S': 4,
+    'T': 6,
+    'U': 4,
+    'V': 2,
+    'W': 2,
+    'X': 1,
+    'Y': 2,
+    'Z': 1
+}
+# dictionary of letters and their points values
+
+SCORES = {
+    ("A", "E", "I", "O", "U", "L", "N", "R", "S", "T"): 1,
+    ("D", "G"): 2,
+    ("B", "C", "M", "P"): 3,
+    ("F", "H", "V", "W", "Y"): 4,
+    ("K"): 5,
+    ("J", "X"): 8,
+    ("Q", "Z"): 10
+}
+
 
 def draw_letters():
-    # dictionary of letters and their frequency
-    LETTER_POOL = {
-        'A': 9,
-        'B': 2,
-        'C': 2,
-        'D': 4,
-        'E': 12,
-        'F': 2,
-        'G': 3,
-        'H': 2,
-        'I': 9,
-        'J': 1,
-        'K': 1,
-        'L': 4,
-        'M': 2,
-        'N': 6,
-        'O': 8,
-        'P': 2,
-        'Q': 1,
-        'R': 6,
-        'S': 4,
-        'T': 6,
-        'U': 4,
-        'V': 2,
-        'W': 2,
-        'X': 1,
-        'Y': 2,
-        'Z': 1
-    }
+
     # build the letter bank
     letter_bank = []
     for letter in LETTER_POOL:
@@ -53,7 +66,17 @@ def uses_available_letters(word, letter_bank):
 
 
 def score_word(word):
-    pass
+
+    sum = 0
+    word = word.upper()
+    if len(word) > 6:
+        sum += 8
+    for letter in word:
+        for key in SCORES:
+            for char in key:
+                if letter == char:
+                    sum += SCORES[key]
+    return sum
 
 
 def get_highest_word_score(word_list):
