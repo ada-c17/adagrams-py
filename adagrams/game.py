f526f4fc9a1389d3c598932ee297db9466762266
Vida Maleki <vida.basmani@gmail.com>
2022-03-29 09:55:19 -0700

    adding function uses_available_letters and passed wave 02 tests

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,8 +1,16 @@
 def draw_letters():
     pass
 
+import copy
 def uses_available_letters(word, letter_bank):
-    pass
+    letter_bank_copy = copy.deepcopy(letter_bank)
+    for letter in word:
+        letter = letter.upper()
+        if letter in letter_bank_copy:
+            letter_bank_copy.remove(letter)
+        else:
+            return False
+    return True
 
 def score_word(word):
     pass
