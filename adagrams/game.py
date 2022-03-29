521a31545dacc812d2e6c432c22f4b8200754ca0
HaileyMatz <hmatz525@gmail.com>
2022-03-29 15:09:42 -0700

    wave3 completed

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,7 +1,6 @@
 import copy 
 import random
 
-
 LETTER_POOL = {
     'A': 9, 
     'B': 2, 
@@ -41,7 +40,6 @@ def draw_letters():
             no_change_letters[random_letter] -= 1
     return ten_letters
      
-
 def uses_available_letters(word, letter_bank):
     letter_count = 0
     letter_copy = letter_bank[:]
@@ -51,35 +49,18 @@ def uses_available_letters(word, letter_bank):
             letter_count += 1
     return letter_count == len(word)
             
- 
-
 def score_word(word):
-    sum = 0
-    for char in word:
-        if len(char) >= 7 and len(char) <= 10:
-            sum += 8
-    return sum
-          
-          
-          
-          
-            # if "A" or "E" or "I" or "O" or "U" or "L" or "N" or "R" or "S" or "T" == char:
-            #     sum += 1
-            # elif "D" or "G" == char:
-            #     sum += 2
-            # elif "B" or "C" or "M" or "P" == char:
-            #     sum += 3
-            # elif "F" or "H" or "V" or "W" or "Y" == char:
-            #     sum += 4
-            # elif "K" == char:
-            #     sum += 5
-            # elif "J" or "X" == char:
-            #     sum += 8
-            # else:
-            #     sum += 10
-
-
-
+    score = 0
+    word = word.upper()
+    score_dict = {"A":1, "E":1, "I":1, "O":1, "U":1, "L":1, "N":1, "R":1, "S":1, "T":1,\
+         "D":2, "G":2, "B":3, "C":3, "M":3, "P":3, "F":4, "H":4, "V":4, "W":4, "Y":4, \
+             "K": 5, "J":8, "X":8, "Q":10, "Z":10}
+    if len(word) >= 7:
+        score = 8
+    for letter in word:
+        score_dict.keys() == letter
+        score += score_dict.get(letter)
+    return score
 
 def get_highest_word_score(word_list):
     pass
