0652f3782e582715b9bc3af742da34e7ba2a89dc
Amel <amelntamark@gmail.com>
2022-03-28 21:47:21 -0700

    implement draw_letters()

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,5 +1,56 @@
+import random
+
+# Return list of ten strings
+# Each string is one letter
+# Import random
+
+# Create dictionary 
+# Create list
+# Iterate 10 times
+# If the number in the list equal to number in dict, continue without adding
+### If not, append to list
+
+
 def draw_letters():
-    pass
+    LETTER_POOL = {
+    'A': 9, 
+    'B': 2, 
+    'C': 2, 
+    'D': 4, 
+    'E': 12, 
+    'F': 2, 
+    'G': 3, 
+    'H': 2, 
+    'I': 9, 
+    'J': 1, 
+    'K': 1, 
+    'L': 4, 
+    'M': 2, 
+    'N': 6, 
+    'O': 8, 
+    'P': 2, 
+    'Q': 1, 
+    'R': 6, 
+    'S': 4, 
+    'T': 6, 
+    'U': 4, 
+    'V': 2, 
+    'W': 2, 
+    'X': 1, 
+    'Y': 2, 
+    'Z': 1
+}
+    letters = []
+   
+    while len(letters) < 10:
+        letter = random.choice(list(LETTER_POOL))
+        if LETTER_POOL[letter] == 0:
+            continue
+        else:
+            letters.append(letter)
+            LETTER_POOL[letter] -= 1
+   
+    return letters
 
 def uses_available_letters(word, letter_bank):
     pass
