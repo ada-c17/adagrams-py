099f658882a0fa7b9cffc8e1e114aeab5a318876
Ivana Maldonado <ivana.a.maldonado@gmail.com>
2022-03-28 23:01:52 -0400

    Final project.

--- adagrams/game.py
+++ adagrams/game.py
@@ -2,28 +2,30 @@ from random import randint
 
 def draw_letters():
     #define list of available letters
+    #take 10 random rolls each with one less number
+    #pop each out between rolls and store in list
+    #return list of rolled letters
     letter_bank = ['A','A','A','A','A','A','A','A','A','B','B','C','C','D','D','D','D','E','E','E','E','E','E','E','E','E','E','E','E','F','F','G','G','G','H','H','I','I','I','I','I','I','I','I','I','J','K','L','L','L','L','M','M','N','N','N','N','N','N','O','O','O','O','O','O','O','O','P','P','Q','R','R','R','R','R','R','S','S','S','S','T','T','T','T','T','T','U','U','U','U','V','V','W','W','X','Y','Y','Z']
+
     letter_list = []
+
     for roll in range(0,10):
-        #take 10 random rolls each with one less number
-        
         remove_letter = randint(0, len(letter_bank)-1)
-        
-        #pop each out between rolls and store in list
         letter = letter_bank.pop(remove_letter)
         letter_list.append(letter)
-    #return list of rolled letters
     return letter_list
+
 
 def uses_available_letters(word, letter_bank):
     #make input uppercase
-    word = word.upper()
     #make copy of list of letters
-    letter_bank_copy = letter_bank.copy()
     #loop through the word index string
     #loop through letter list and pop found letter
     #if letter not found return false
     #return true
+    word = word.upper()
+    letter_bank_copy = letter_bank.copy()
+
     for letter in word:
         if letter not in letter_bank_copy:
             return False
@@ -44,23 +46,25 @@ def score_word(word):
     #  lookup point value in dictionary
     #  add to sum
     #return sum
-    # if len(word) == 0:
-    #     return None
-    score_dict = {  'A' : 1, 'E' : 1, 'I' : 1, 'O' : 1, 'U' : 1, 'L' : 1, 'N' : 1, 'R' : 1, 'S' : 1, 'T' : 1,
+    
+    score_dict = {  'A' : 1, 'E' : 1, 'I' : 1, 'O' : 1, 'U' : 1, 
+                    'L' : 1, 'N' : 1, 'R' : 1, 'S' : 1, 'T' : 1,
                     'D' : 2, 'G' : 2,
                     'B' : 3, 'C' : 3, 'M' : 3, 'P' : 3,
                     'F' : 4, 'H' : 4, 'V' : 4, 'W' : 4, 'Y' : 4,
                     'K' : 5,
                     'J' : 8, 'X' : 8,
                     'Q' : 10, 'Z' : 10}
+
     word = word.upper()
     score = 0
+
     if len(word) > 6:
         score += 8
     for letter in word:
         score += score_dict[letter]
     return score
-    pass
+    
 
 def get_highest_word_score(word_list):
     #define word,score tuple
@@ -70,20 +74,20 @@ def get_highest_word_score(word_list):
     #  compare length is same
     #    replace tuple with index word and score
     #return tuple
-    highest_score = ["", 0]
+    highest_score = ("",0)
     score = 0
 
     for word in word_list:
         score = score_word(word)
         if score > highest_score[1]:
-            highest_score = [word, score]
+            highest_score = word, score
         elif score == highest_score[1]:
             if len(highest_score[0]) == 10:
                 continue
             elif len(word) == 10: 
-                highest_score = [word, score]
+                highest_score = word, score
             elif len(highest_score[0]) > len(word):
-                highest_score = [word, score]
+                highest_score = word, score
 
     return highest_score
 
