0bf8590db56f7b5d1db2ee31ea0de869c156324e
Katina Carranza <katina.carranza@gmail.com>
2022-03-28 17:39:16 -0700

    modified available_letters dict to add letter score value and built out score_word to implement change

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,49 +1,44 @@
 import random 
 available_letters = {
-    'A': 9, 
-    'B': 2, 
-    'C': 2, 
-    'D': 4, 
-    'E': 12, 
-    'F': 2, 
-    'G': 3, 
-    'H': 2, 
-    'I': 9, 
-    'J': 1, 
-    'K': 1, 
-    'L': 4, 
-    'M': 2, 
-    'N': 6, 
-    'O': 8, 
-    'P': 2, 
-    'Q': 1, 
-    'R': 6, 
-    'S': 4, 
-    'T': 6, 
-    'U': 4, 
-    'V': 2, 
-    'W': 2, 
-    'X': 1, 
-    'Y': 2, 
-    'Z': 1
+    'A': [9, 1],
+    'B': [2,3], 
+    'C': [2, 3], 
+    'D': [4, 2], 
+    'E': [12, 1], 
+    'F': [2, 4], 
+    'G': [3, 2], 
+    'H': [2, 4], 
+    'I': [9, 1], 
+    'J': [1, 8], 
+    'K': [1, 5], 
+    'L': [4, 1], 
+    'M': [2, 3], 
+    'N': [6, 1], 
+    'O': [8, 1], 
+    'P': [2,3], 
+    'Q': [1,10], 
+    'R': [6,1], 
+    'S': [4,1], 
+    'T': [6,1], 
+    'U': [4,1], 
+    'V': [2,4], 
+    'W': [2,4], 
+    'X': [1,8], 
+    'Y': [2,4], 
+    'Z': [1,10]
 }
 def draw_letters():
     letter_hand = []
     while len(letter_hand) < 10:
         picked_letter = random.choice(list(available_letters.keys()))
         # check frequency
-        if letter_hand.count(picked_letter) < available_letters[picked_letter]:
+        if letter_hand.count(picked_letter) < available_letters[picked_letter][0]:
             letter_hand.append(picked_letter)
     return letter_hand    
 
 print(draw_letters())
 
 def uses_available_letters(word, letter_bank):
-    # create a copy of letter_bank
-    # for letter in word, if letter in letter_bank:
-    # remove letter from letter_bank
-    # else: return false
-    # return True
     letter_bank_copy = letter_bank[:]
     word_uppercase = word.upper()
 
@@ -54,11 +49,16 @@ def uses_available_letters(word, letter_bank):
             return False
     return True
 
-# print(uses_available_letters("FED",['E', 'J', 'D', 'W', 'S', 'L', 'C', 'W', 'D', 'F']))
-# print(uses_available_letters("feD",['E', 'J', 'D', 'W', 'S', 'L', 'C', 'W', 'D', 'F']))
-
 def score_word(word):
-    pass
+    score = 0
+    word_uppercase = word.upper()
+    for letter in word_uppercase:
+        score += available_letters[letter][1]
+    if len(word_uppercase) >= 7:
+        score += 8
+    return score
+
+# print(score_word("aaaaaax"))
 
 def get_highest_word_score(word_list):
     pass
