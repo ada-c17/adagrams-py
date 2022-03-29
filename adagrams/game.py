1259dc5925dffed7d8a9b0160b680aceddc6fc9a
sarah <skiyoko14@gmail.com>
2022-03-29 14:45:38 -0700

    removed unused variables, created and used global variables to represent constants

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,3 +1,4 @@
+from ctypes.wintypes import LONG
 import random
 import copy
 
@@ -30,6 +31,20 @@ LETTER_POOL = {
     'Z': 1
 }
 
+LETTER_VALUES = {
+    "A" : 1, "E" : 1, "I" : 1, "O" : 1, "U" : 1,
+    "L" : 1, "N" : 1, "R" : 1, "S" : 1, "T" : 1, 
+    "D" : 2, "G" : 2,
+    "B" : 3, "C" : 3, "M" : 3, "P" : 3,
+    "F" : 4, "H" : 4, "V" : 4, "W" : 4, "Y" : 4,
+    "K" : 5,
+    "J" : 8, "X" : 8,
+    "Q" : 10,"Z" : 10
+    }
+
+LONG_WORD_BONUS = 8
+LONG_WORD_RANGE = (7, 8, 9, 10)
+
 # build a hand of 10 letters
 def draw_letters():
     draw_pool = []
@@ -38,12 +53,12 @@ def draw_letters():
         draw_pool.extend([key]*value)
     # create hand with sample of random letters from draw_pool
     hand = random.sample(draw_pool, 10)
-    return(hand)
+    return hand
 
 def uses_available_letters(word, letter_bank):
     available_letters = letter_bank.copy()
+    word = word.upper()
     for letter in word:
-        letter = letter.upper()
         if letter in available_letters:
             available_letters.remove(letter)
         else:
@@ -53,51 +68,24 @@ def uses_available_letters(word, letter_bank):
 # scores word based on letter values, bonus of 8 points added for words 7-10 letters long
 def score_word(word):
     score = 0
-    letter_vals = {"A" : 1,
-    "E" : 1,
-    "I" : 1,
-    "O" : 1,
-    "U" : 1,
-    "L" : 1,
-    "N" : 1,
-    "R" : 1,
-    "S" : 1,
-    "T" : 1,
-    "D" : 2,
-    "G" : 2,
-    "B" : 3,
-    "C" : 3,
-    "M" : 3,
-    "P" : 3,
-    "F" : 4,
-    "H" : 4,
-    "V" : 4,
-    "W" : 4,
-    "Y" : 4,
-    "K" : 5,
-    "J" : 8,
-    "X" : 8,
-    "Q" : 10,
-    "Z" : 10
-    }
+
     for letter in word:
         letter = letter.upper()
-        if letter in letter_vals:
-            score += letter_vals[letter]
+        if letter in LETTER_VALUES:
+            score += LETTER_VALUES[letter]
         else:
             continue
-    if len(word) > 6 and len(word) < 11:
-        score += 8
+    if len(word) in LONG_WORD_RANGE:
+        score += LONG_WORD_BONUS
+
     return score
 
 def get_highest_word_score(word_list):
-    high_score = 0
-    
     word_dict = {}
     for word in word_list:
         word_score = score_word(word)
         word_dict[word] = word_score
-    
+
     highest_scoring_word = max(word_dict, key=word_dict.get)
     high_score = word_dict[highest_scoring_word]
 
