233ff992afa912cf5c18cea14d0161609a9068ae
Emily Schell <lauraemilyschell@gmail.com>
2022-03-28 20:32:25 -0400

    modify functions 2 and 3 to work in all uppercase instead of lowercase

--- adagrams/game.py
+++ adagrams/game.py
@@ -6,9 +6,9 @@ def uses_available_letters(word, letter_bank):
     letters_valid = True
     letter_bank_copy = []
     for letter in letter_bank:
-        letter_bank_copy.append(letter.lower())
+        letter_bank_copy.append(letter.upper())
     for letter in word:
-        letter = letter.lower()
+        letter = letter.upper()
         if letter in letter_bank_copy:
             letter_bank_copy.remove(letter)
         else:
@@ -19,24 +19,24 @@ def uses_available_letters(word, letter_bank):
 def score_word(word):
 
     points_dict = {}
-    for letter in ["a", "e", "i", "o", "u", "l", "n", "r", "s", "t"]:
+    for letter in ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"]:
         points_dict[letter] = 1
-    for letter in ["d", "g"]:
+    for letter in ["D", "G"]:
         points_dict[letter] = 2
-    for letter in ["b", "c", "m", "p"]:
+    for letter in ["B", "C", "M", "P"]:
         points_dict[letter] = 3
-    for letter in ["f", "h", "v", "w", "y"]:
+    for letter in ["F", "H", "V", "W", "Y"]:
         points_dict[letter] = 4
-    for letter in ["k"]:
+    for letter in ["K"]:
         points_dict[letter] = 5
-    for letter in ["j", "x"]:
+    for letter in ["J", "X"]:
         points_dict[letter] = 8
-    for letter in ["q", "z"]:
+    for letter in ["Q", "Z"]:
         points_dict[letter] = 10
 
     score = 0
 
-    for char in word.lower():
+    for char in word.upper():
         if char not in points_dict:
             continue
         else:
