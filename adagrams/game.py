4d3107a87e3a80a8ce9d8533d278436708018eb0
Morgan <morganheuer@gmail.com>
2022-03-29 13:50:04 -0500

    implemented score_word function wave 3

--- adagrams/game.py
+++ adagrams/game.py
@@ -20,7 +20,44 @@ def uses_available_letters(word, letter_bank):
 
 
 def score_word(word):
-    pass
+    score_dictionary = {
+        "A": 1, 
+        "E": 1, 
+        "I": 1, 
+        "O": 1, 
+        "U": 1, 
+        "L": 1, 
+        "N": 1, 
+        "R": 1,
+        "S": 1,
+        "T": 1,
+        "D": 2,
+        "G": 2,
+        "B": 3,
+        "C": 3,
+        "M": 3,
+        "P": 3,
+        "F": 4,
+        "H": 4,    
+        "V": 4,
+        "W": 4,
+        "Y": 4,
+        "K": 5,
+        "J": 8,
+        "X": 8,
+        "Q": 10,
+        "Z": 10
+    }
+    score = 0
+    for i in range(len(word)):
+        char = word[i]
+        if char.islower():
+            char = char.upper()
+        if char in score_dictionary:
+            score += score_dictionary[char]
+    if len(word) >6:
+        score += 8
+    return score
 
 def get_highest_word_score(word_list):
     pass
