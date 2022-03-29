f0782743f28dfced374c0389e52d72eedb36690d
Olena <olenazachkow@gmail.com>
2022-03-29 14:23:47 -0700

    commit

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,9 +1,41 @@
 #testing commit 
-def draw_letters():
-    pass
+LETTER_POOL = {
+    'A': 9, 
+    'B': 2, 
+    'C': 2, 
+    'D': 4, 
+    'E': 12, 
+    'F': 2, 
+    'G': 3, 
+    'H': 2, 
+    'I': 9, 
+    'J': 1, 
+    'K': 1, 
+    'L': 4, 
+    'M': 2, 
+    'N': 6, 
+    'O': 8, 
+    'P': 2, 
+    'Q': 1, 
+    'R': 6, 
+    'S': 4, 
+    'T': 6, 
+    'U': 4, 
+    'V': 2, 
+    'W': 2, 
+    'X': 1, 
+    'Y': 2, 
+    'Z': 1
+}
 
+from copy import deepcopy
+from operator import le
+import random
+
+def draw_letters():
+   pass
 def uses_available_letters(word, letter_bank):
-    pass
+   pass
 
 def score_word(word):
     pass
