2b619153d1393049b94b5ae19875b2e1627e73da
Sreymompech <sanchhen2013@gmail.com>
2022-03-28 21:38:37 -0700

    Adding comments into each function of Wave 1 and Wave 2

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,6 +1,3 @@
-import string
-import random
-
 
 LETTER_POOL = {
     'A': 9, 
@@ -31,24 +28,55 @@ LETTER_POOL = {
     'Z': 1
 }
 
+letter_point ={
+    ("A", "E", "I", "O", "U", "L", "N", "R", "S", "T"): 1,
+    ("D", "G"): 2,
+    ("B", "C", "M", "P"): 3,
+    ("F", "H", "V", "W", "Y"): 4,
+    ("K"): 5,
+    ("J", "X"): 8,
+    ("Q", "Z"): 10
+}
+
+import random
+
+
 def draw_letters():
+    """Drawing 10 letters from a pool of letter and return a list of that letters"""
+    # a list to store all letters from a pool of letter
     list_letter = []
+    # loop over LETTER_POOL to get key and value
+    # key will be the letter adding to list and 
+    # value is number of times that the letter will be adding
     for letter, count in LETTER_POOL.items():
-        for i in range(0, count):
+        # adding letter into list depend on value of count 
+        for i in range(count):
             list_letter.append(letter)
+    # create a list that randomly draw 10 letters from list_letter 
     letter_draw = random.sample(list_letter, k=10)
     
     return letter_draw
 
 
 def uses_available_letters(word, letter_bank):
+    """
+    Checking each letter of word:
+        - return False if it is not in letter_bank
+        - return True if it is in letter_banks
+    """
+    # create a new list which same as letter_bank, so it doen't effect to original when editing
     letter_bank_copy = letter_bank.copy()
+    # loop over word to get each character
     for char in word.upper():
+        # return False if the character of word not in the new created list
         if char not in letter_bank_copy:
             return False
-        else:
+        # otherwise removing each character of word from letter_bank_copy when it is in there
+        # return True after removing all characters of word that is in letter_bank_copy
+        else: 
             letter_bank_copy.remove(char)
     return True
+
 
 def score_word(word):
     pass
