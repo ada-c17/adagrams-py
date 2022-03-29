616b8ff0b3d01995daf5df347fc64eac8389d89a
Thu Vuong <tavuongg@gmail.com>
2022-03-29 11:23:24 -0700

    figured out how to import and created constants file to host data, init file is needed for correct import

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,34 +1,5 @@
-from random import choice, randint, shuffle
-from copy import deepcopy
-
-LETTER_POOL = {
-    'A': 9, 
-    'B': 2, 
-    'C': 2, 
-    'D': 4, 
-    'E': 12, 
-    'F': 2, 
-    'G': 3, 
-    'H': 2, 
-    'I': 9, 
-    'J': 1, 
-    'K': 1, 
-    'L': 4, 
-    'M': 2, 
-    'N': 6, 
-    'O': 8, 
-    'P': 2, 
-    'Q': 1, 
-    'R': 6, 
-    'S': 4, 
-    'T': 6, 
-    'U': 4, 
-    'V': 2, 
-    'W': 2, 
-    'X': 1, 
-    'Y': 2, 
-    'Z': 1
-}
+from random import shuffle
+from adagrams import LETTER_POOL
 
 def create_letter_pool():
     letter_pool = []
