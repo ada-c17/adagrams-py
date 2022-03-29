5e325970e66ec69dc2e257bcff928b7746be6bcb
Thu Vuong <tavuongg@gmail.com>
2022-03-28 21:34:48 -0700

    refactored draw_letters with shuffle method

--- adagrams/game.py
+++ adagrams/game.py
@@ -30,7 +30,6 @@ LETTER_POOL = {
     'Z': 1
 }
 
-# create a list holding all 98 letters (letter_bank)
 def create_letter_pool():
     letter_pool = []
     for letter, frequency in LETTER_POOL.items():
@@ -39,28 +38,13 @@ def create_letter_pool():
     
     return letter_pool
 
-# thu's suggestion for improving draw_letters():
-# def draw_letters():
-
-#     # it will restart every time this function is invoked
-#     letter_pool = create_letter_pool()
-#     shuffle(letter_pool)
-#     user_hand = letter_pool[0:10]
-
-#     return user_hand
-
 def draw_letters():
-    user_hand = []
-    copy_of_letter_bank = deepcopy(letter_bank)
 
-    for i in range(10):
-        index = randint(0, len(copy_of_letter_bank) - 1)
-        letter = copy_of_letter_bank.pop(index)
-        user_hand.append(letter)
+    letter_pool = create_letter_pool()
+    shuffle(letter_pool)
+    user_hand = letter_pool[0:10]
 
-#     for i in range(11):
-#         if 
-#         user_hand.append(choice(letter_pool))
+    return user_hand
 
 def uses_available_letters(word, letter_bank):
     pass
