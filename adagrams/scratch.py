508d92041329715a26658e1e53c24f97b52105f3
Lauren Kleinert <lauren.o.kleinert@gmail.com>
2022-03-29 15:24:29 -0700

    Added Wave 3 outline to scratch.py file

--- adagrams/scratch.py
+++ adagrams/scratch.py
@@ -18,22 +18,64 @@ import random
 #     return letter_list
 
 # draw_letters(LETTERS_POOL)
-letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
+# letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
 
-def uses_available_letters(word, letter_bank):
-    list_copy = letter_bank.copy()
+# def uses_available_letters(word, letter_bank):
+#     list_copy = letter_bank.copy()
 
-    print(word)
+#     print(word)
 
-    #for each character in word
-    for character in word:
+#     #for each character in word
+#     for character in word:
 
-        character = character.capitalize()
+#         character = character.capitalize()
         
-        if character in letter_bank:
-            list_copy.remove(character)
-        else:
-            return False
-    return True
+#         if character in letter_bank:
+#             list_copy.remove(character)
+#         else:
+#             return False
+#     return True
 
-print(uses_available_letters("ABCD", letters))
+# print(uses_available_letters("ABCD", letters))
+
+def word_score(word):
+    letter_score = {
+        "A": 1, 
+        "E": 1, 
+        "I": 1, 
+        "O": 1,
+        "U": 1, 
+        "L": 1, 
+        "N": 1, 
+        "R": 1,
+        "S": 1,
+        "T": 1, 
+        "D": 2, 
+        "G": 2, 
+        "B": 3,
+        "C": 3,
+        "M": 3,
+        "P": 3, 
+        "F": 4, 
+        "H": 4, 
+        "V": 4,
+        "W": 4,
+        "Y": 4,
+        "K": 5,
+        "J": 8,
+        "X": 8,
+        "Q": 10,
+        "Z": 10
+    }
+
+    extra_points= {8: [7, 8, 9, 10]}
+    total = 0
+    
+    if len(word) in extra_points.values():
+        total += 8
+    for letter in word:
+        total += letter_score[letter]
+        #can't return a tuple here- (i.e. total, word)
+    return total
+
+print(word_score("cat"))
