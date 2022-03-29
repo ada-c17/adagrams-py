08f0e3ec45b4e28a47f19d4079ec379f7f25f46d
Ying G <yingg@yings-air.lan>
2022-03-29 15:49:50 -0700

    pass wave 4 tests

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,3 +1,4 @@
+from collections import Counter
 import random
 
 LETTER_POOL = {
@@ -29,6 +30,46 @@ LETTER_POOL = {
     'Z': 1
 }
 
+POINT_SYSTEM = {
+    'A':1,
+    'B':3,
+    'C':3,
+    'D':2,
+    'E':1,
+    'F':4,
+    'G':2,
+    'H':4,
+    'I':1,
+    'J':8,
+    'K':5,
+    'L':1,
+    'M':3,
+    'N':1,
+    'O':1,
+    'P':3,
+    'Q':10,
+    'R':1,
+    'S':1,
+    'T':1,
+    'U':1,
+    'V':4,
+    'W':4,
+    'X':8,
+    'Y':4,
+    'Z':10
+}
+
+
+
+# |Letter                        | Value|
+# |:----------------------------:|:----:|
+# |A, E, I, O, U, L, N, R, S, T  |   1  |
+# |D, G                          |   2  |
+# |B, C, M, P                    |   3  |
+# |F, H, V, W, Y                 |   4  |
+# |K                             |   5  |
+# |J, X                          |   8  |
+# |Q, Z                          |   10 |
 
 def draw_letters():
     # copy a dictionary so that we don't change the data, it's constant
@@ -41,11 +82,49 @@ def draw_letters():
             LETTER_POOL_COPY[letter_drawn] -= 1 
     return letters_drawn
 
+
 def uses_available_letters(word, letter_bank):
-    pass
+    upper_word = word.upper()
+    copy_letter_bank = letter_bank.copy()
+    for char in upper_word:
+        if char not in copy_letter_bank:
+            return False
+        else:
+            copy_letter_bank.remove(char) 
+    return True
+    
 
 def score_word(word):
-    pass
+    points = 0
+    for char in word:
+        points += POINT_SYSTEM[char.upper()]
+    if len(word) >= 7 and len(word) <= 10:
+        points += 8
+    return points
+    
 
 def get_highest_word_score(word_list):
-    pass
+    max_word = None
+    max_score = 0
+    for word in word_list:
+        word_score = score_word(word)
+        if word_score > max_score:
+            max_word = word
+            max_score = word_score
+        elif word_score == max_score:
+            if type(max_word) != list:
+                max_word = list(max_word)
+                max_word.append(word)
+            else:
+                max_word.append(word)
+    if type(max_word) == str:
+        return (max_word, max_score)
+
+    min_len = 100
+    min_word = None
+    for word in word_list:
+        if len(word) == 10:
+            return (word,score_word(word))
+        elif len(word) < min_len:
+            min_word, min_len = word, len(word)
+    return (min_word,score_word(min_word))
