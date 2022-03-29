84a645c28b346adf1bede219908ede958e345d3d
Natascha <njenkns@gmail.com>
2022-03-29 15:32:36 -0700

    Attempting to pull files

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,4 +1,5 @@
 import random 
+from collections import Counter
 
 def draw_letters():
     # Returns an array of ten strings - letters should be randomly drawn from a pool of letters
@@ -31,10 +32,25 @@ def draw_letters():
             'X': 1, 
             'Y': 2, 
             'Z': 1
-        }
+    }
 
-    pool_of_letters = random.choices(list(all_available_letters.keys()), weights=all_available_letters.values(), k = 10)
-    return pool_of_letters
+    output = []
+
+    while len(output) < 10:
+        letter_frequency = Counter(output)
+        print(letter_frequency)
+        selected_letter = random.choices(list(all_available_letters.keys()), weights=all_available_letters.values(), k = 1)
+        print(selected_letter)
+    #     for letter, weight in letter_frequency.items():
+    #         if letter in letter_frequency and weight > all_available_letters[letter]:
+    #             continue
+    #         else:
+    #             output.extend(selected_letter)
+    # print(output)  
+    # return output
+
+    # selected_letters = random.choices(list(all_available_letters.keys()), weights=all_available_letters.values(), k = 10)
+    # return selected_letters
 
 def uses_available_letters(word, letter_bank):
     pass
