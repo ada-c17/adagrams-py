4455abfabaa26d420f91854d72e6a4550c96ed15
Juli <juli.terapin@gmail.com>
2022-03-29 17:57:28 -0400

    Added tests for wave 3

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,4 +1,3 @@
-from multiprocessing.sharedctypes import Value
 import random
 
 '''
@@ -35,19 +34,20 @@ This method should have the following properties:
 | M : 2  | Z : 1 |
 '''
 
-LETTER_QUANTITY_DICT = {'A' : 9, 'N' : 6 ,
-'B' : 2, 'O' : 8, 
-'C' : 2, 'P' : 2, 
-'D' : 4, 'Q' : 1, 
-'E' : 12,'R' : 6,
-'F' : 2, 'S' : 4, 
-'G' : 3, 'T' : 6,
-'H' : 2, 'U' : 4,
-'I' : 9, 'V' : 2, 
-'J' : 1, 'W' : 2,
-'K' : 1, 'X' : 1, 
-'L' : 4, 'Y' : 2,
-'M' : 2, 'Z' : 1}
+LETTER_QUANTITY_DICT = {
+    'A' : 9, 'N' : 6 ,
+    'B' : 2, 'O' : 8, 
+    'C' : 2, 'P' : 2, 
+    'D' : 4, 'Q' : 1, 
+    'E' : 12,'R' : 6,
+    'F' : 2, 'S' : 4, 
+    'G' : 3, 'T' : 6,
+    'H' : 2, 'U' : 4,
+    'I' : 9, 'V' : 2, 
+    'J' : 1, 'W' : 2,
+    'K' : 1, 'X' : 1, 
+    'L' : 4, 'Y' : 2,
+    'M' : 2, 'Z' : 1}
 
 def draw_letters():
     letter_list = []
@@ -118,31 +118,7 @@ def is_in_list(word, letter_bank):
         return False
 
 
-""" ### Wave 3: score_word
-
-Now you need a function returns the score of a given word as defined by the Adagrams game.
-
-Implement the function `score_word` in `game.py`. This method should have the following properties:
-
-- Has one parameter: `word`, which is a string of characters
-- Returns an integer representing the number of points
-- Each letter within `word` has a point value. The number of points of each 
-letter is summed up to represent the total score of `word`
-- Each letter's point value is described in the table below
-- If the length of the word is 7, 8, 9, or 10, then the 
-word gets an additional 8 points
-
-#### Score chart
-
-|Letter                        | Value|
-|:----------------------------:|:----:|
-|A, E, I, O, U, L, N, R, S, T  |   1  |
-|D, G                          |   2  |
-|B, C, M, P                    |   3  |
-|F, H, V, W, Y                 |   4  |
-|K                             |   5  |
-|J, X                          |   8  |
-|Q, Z                          |   10 | """
+# Wave 3
 
 def score_word(word):
     score_chart = {
