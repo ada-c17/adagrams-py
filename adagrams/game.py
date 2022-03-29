2c031c25d1c79be774a886e229e0296ef2d1a985
Huma Hameed <mrskq1@gmail.com>
2022-03-29 10:50:41 -0400

    recovered first wave

--- adagrams/game.py
+++ adagrams/game.py
@@ -32,38 +32,24 @@ LETTER_POOL = {
 }
 
 def draw_letters():
-    return_string = []
-    LETTER_POOL = {
-    'A': 9, 
-    'B': 2, 
-    'C': 2, 
-    'D': 4, 
-    'E': 12, 
-    'F': 2, 
-    'G': 3, 
-    'H': 2, 
-    'I': 9, 
-    'J': 1, 
-    'K': 1, 
-    'L': 4, 
-    'M': 2, 
-    'N': 6, 
-    'O': 8, 
-    'P': 2, 
-    'Q': 1, 
-    'R': 6, 
-    'S': 4, 
-    'T': 6, 
-    'U': 4, 
-    'V': 2, 
-    'W': 2, 
-    'X': 1, 
-    'Y': 2, 
-    'Z': 1
-}  
+    ten_letters = []
+    no_change_letters = LETTER_POOL.copy()
+    for i in range(10):
+        random_letter = random.choice(list(LETTER_POOL))
+        if no_change_letters[random_letter] > 0:
+            ten_letters.append(random_letter)
+            no_change_letters[random_letter] -= 1
+    return ten_letters
+     
 
 def uses_available_letters(word, letter_bank):
-    pass
+    for character in word.upper():
+        if character in letter_bank:
+            # return True
+            if word.isalpha():
+                return True
+            else:
+                return False
 
 def score_word(word):
     pass
