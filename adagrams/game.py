f1aa2ae71bb7082a34303483050aff27c9338326
San <san.robinxin@gmail.com>
2022-03-29 15:07:09 -0500

    updated

--- adagrams/game.py
+++ adagrams/game.py
@@ -36,37 +36,10 @@ LETTER_POOL = {
 
 
 def draw_letters():
-    letter_pool = {
-    'A': 9, 
-    'B': 2, 
-    'C': 2, 
-    'D': 4, 
-    'E': 12, 
-    'F': 2, 
-    'G': 3, 
-    'H': 2, 
-    'I': 9, 
-    'J': 1, 
-    'K': 1, 
-    'L': 4, 
-    'M': 2, 
-    'N': 6, 
-    'O': 8, 
-    'P': 2, 
-    'Q': 1, 
-    'R': 6, 
-    'S': 4, 
-    'T': 6, 
-    'U': 4, 
-    'V': 2, 
-    'W': 2, 
-    'X': 1, 
-    'Y': 2, 
-    'Z': 1
-}
+ 
     letters = []
 
-    letter_bag = list(Counter(letter_pool).elements())
+    letter_bag = list(Counter(LETTER_POOL).elements())
     shuffle(letter_bag)
 
     for letter in range(10):
@@ -75,7 +48,12 @@ def draw_letters():
 
 
 def uses_available_letters(word, letter_bank):
-    pass
+    for letter in word:
+        if letter not in letter_bank:
+            return False
+        else:
+            return True 
+
 
 def score_word(word):
     pass
