d6ead05eecc02c71a21cf59792a18d94c7c1d809
Daria <daria.iakymenko@gmail.com>
2022-03-29 14:20:24 -0700

    added score_word function, passed tests for wave 3

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,7 +1,7 @@
 
 from operator import is_
 import random
-
+import copy
 
 LETTER_POOL = {
     'A': 9, 
@@ -31,7 +31,6 @@ LETTER_POOL = {
     'Y': 2, 
     'Z': 1}
 
-
 def draw_letters():
     pool_list = []
     for letter, frequency in LETTER_POOL.items():
@@ -41,7 +40,6 @@ def draw_letters():
     random_letters_list = random.sample(pool_list, 10)
     return random_letters_list
 
-import copy
 def uses_available_letters(word, letter_bank):
     letter_bank_copy = copy.deepcopy(letter_bank)
     for letter in word:
@@ -53,7 +51,14 @@ def uses_available_letters(word, letter_bank):
     return True
 
 def score_word(word):
-    pass
+    score_dict = {'A': 1, 'B': 3, 'C': 3, 'D': 2, 'E': 1, 'F': 4, 'G': 2, 'H': 4, 'I': 1, 'J': 8, 'K': 5, 'L': 1, 'M': 3, 'N': 1, 'O': 1, 'P': 3, 'Q': 10, 'R': 1, 'S': 1, 'T': 1, 'U': 1, 'V': 4, 'W': 4, 'X': 8, 'Y': 4, 'Z': 10}
+    sum = 0
+    word = word.upper()
+    for letter in word:
+        sum += score_dict[letter]
+    if len(word) == 7 or len(word) == 8 or len(word) == 9 or len(word) == 10:
+        sum += 8
+    return sum
 
 def get_highest_word_score(word_list):
     pass
