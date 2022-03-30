58a753610a14d59c7ce6f60c226fdcd4f557f8b7
Nishat <nsalsabil25@gmail.com>
2022-03-29 21:38:57 -0700

    created score_word function and passes all wave_03 tests; wave_03 complete

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,37 +1,6 @@
 import random
 import copy 
-from adagrams.constants import LETTER_POOL
-
-'''
-LETTER_POOL = {
-    'A': 9, 
-    'B': 2, 
-    'C': 2, 
-    'D': 4, 
-    'E': 12, 
-    'F': 2, 
-    'G': 3, 
-    'H': 2, 
-    'I': 9, 
-    'J': 1, 
-    'K': 1, 
-    'L': 4, 
-    'M': 2, 
-    'N': 6, 
-    'O': 8, 
-    'P': 2, 
-    'Q': 1, 
-    'R': 6, 
-    'S': 4, 
-    'T': 6, 
-    'U': 4, 
-    'V': 2, 
-    'W': 2, 
-    'X': 1, 
-    'Y': 2, 
-    'Z': 1
-}
-'''
+from adagrams.constants import LETTER_POOL, LETTER_SCORE_DICT
 
 # draw a letter, from the letter pool
 # put each letter in a dictionary, assign a count
@@ -67,7 +36,6 @@ def draw_letters():
 # if letter is not in the hand return False
 # move to the next letter
 
-
 def uses_available_letters(word, hand):
     '''
     input: word (a string) and hand (a list of strings, one char each)
@@ -84,12 +52,33 @@ def uses_available_letters(word, hand):
     return True
 
 
+# assert score_word("A") == 1
+# assert score_word("DOG") == 5
+# assert score_word("WHIMSY") == 17
+
+# make score = 0
+# iterate over each letter in the word
+# find the score of each letter in the score dictionary
+#  If the length of the word is 7, 8, 9, or 10, then the word gets an additional 8 points
+# add the score of the letter to the existing score
+# return score
 
 def score_word(word):
-    pass
-
-
-
+    '''
+    input: word (a string of characters)
+    output: Returns a total score based on the value of each char in
+    word, as defined in LETTER_SCORE_DICT. Words between 7 and 10 
+    char score an extra 8 points.
+    '''
+    score = 0
+    word = word.upper()
+    for letter in word:
+        for key, value in LETTER_SCORE_DICT.items():
+            if letter in value:
+                score += key 
+    if 7 <= len(word) <= 10:
+        score += 8
+    return score
 
 
 
