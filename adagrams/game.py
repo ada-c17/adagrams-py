8d372d119f714fcd95a75a8640441136269f5d7a
Shayla L <88570826+compugrl@users.noreply.github.com>
2022-03-29 19:46:59 -0700

    Added Wave 3 functionality

--- adagrams/game.py
+++ adagrams/game.py
@@ -62,7 +62,48 @@ def uses_available_letters(word, letter_bank):
     return True
 
 def score_word(word):
-    pass
+    letter_vals = {
+        "a": 1, 
+        "b": 3, 
+        "c": 3, 
+        "d": 2, 
+        "e": 1, 
+        "f": 4, 
+        "g": 2, 
+        "h": 4, 
+        "i": 1, 
+        "j": 8, 
+        "k": 5, 
+        "l": 1, 
+        "m": 3, 
+        "n": 1, 
+        "o": 1, 
+        "p": 3, 
+        "q": 10, 
+        "r": 1, 
+        "s": 1, 
+        "t": 1, 
+        "u": 1, 
+        "v": 4, 
+        "w": 4, 
+        "x": 8, 
+        "y": 4, 
+        "z": 10 }
+
+    final_score = 0
+    letter_count = 0
+    length = len(word)
+
+    word = word.lower()
+
+    for char in word:
+        letter_count = letter_vals[char]
+        final_score += letter_count
+
+    if 7 <= length <= 10:
+        final_score += 8   
+
+    return final_score
 
 def get_highest_word_score(word_list):
     pass
