6463d4c7ee63e41b7fab161022edc4d8bb68008a
jae <j.kerr.clayton@gmail.com>
2022-03-29 22:35:06 -0500

    idk, tried something out -- we were counting the frequency of the wrong thing, so i tried idea of making a list of just words that have winning score level ?

--- adagrams/game.py
+++ adagrams/game.py
@@ -146,27 +146,56 @@ def get_highest_word_score(word_list):
 
     word_list_scores = []
     highest_score = 0
-
-
+    best_words = []
 
     
 
     for word in word_list:
-        word_length= len(word)
+        # word_length= len(word)
         word_score = score_word(word)
         word_list_scores.append(word_score)
+        highest_score = max(word_list_scores)
+        if word_score == highest_score: 
+            best_words.append(word)
 
-    for word_score in word_list_scores: 
-        word_score_frequency = word_list_scores.count(word_score)
-        print(f"THIS IS THE WORD SCORE FREQUENCY{word_score_frequency=}")
-        if word_score == max(word_list_scores): 
-            if word_score_frequency > 1: 
-                if word_length == 10: 
+    if len(best_words) == 1: 
+        word = best_words[0]
+        highest_score_tuple = (word, highest_score)
+        return highest_score_tuple
+
+    for word in best_words: 
+        
+        if len(word) == 10: 
+            highest_score_tuple = (word, highest_score)
+        else: 
+            ### this isn't the right way to do it! 
+            ## but thought is that we have a 
+            shortest_word = min(word_list, key=lambda word: len(word))
+            highest_score_tuple = (shortest_word, highest_score)
+
+        
+
+
+
+    # for word_score in word_list_scores: 
+    #     word_score_frequency = word_list_scores.count(word_score)
+    #     highest_score = max(word_list_scores)
+    #     print(f"THIS IS THE WORD SCORE FREQUENCY{word_score_frequency=}")
+    #     if word_score == highest_score: 
+    #         highest_score = word_score
+    #         highest_score_tuple = (word, highest_score)
+    #         if word_score_frequency > 1: 
+    #             if word_length == 10:
                     
-                    highest_score = word_score
-                    highest_score_tuple = (word, highest_score)
+    #                 highest_score_tuple = (word, highest_score)
+    #             else: 
+    #                 shortest_word = min(word_list, key=lambda word: len(word))
+    #                 highest_score_tuple = (shortest_word, highest_score)
 
-                    return highest_score_tuple
+                    
+                    
+    return highest_score_tuple
+                
                 # highest_score.append(word_score)
             # if word_score > highest_word_score: 
 
@@ -205,10 +234,69 @@ def get_highest_word_score(word_list):
     # for user in score_list: 
     #     if user[1] > max_score: 
     #         user[1] = max_score
-    pass 
+ 
         
+# def get_highest_scoring_words(word_list): 
+#     best_score = 0
+#     score_list = []
+#     best_words = []
+#     word_lengths = []
+#     shortest_words = []
+
+#     for word in word_list: 
+#         word_lengths.append(len(word))
+#         smallest_length = min(word_lengths)
+#         if len(word) == smallest_length:
+#             shortest_words.append(word)
 
 
+#     for word in word_list: 
+#         score = score_word(word)
+#         score_list.append(score)
+#         best_score = max(score_list)
+#         if score == best_score: 
+#             best_words.append(word)
+
+#     return tuple(best_words, best_score, shortest_words[0]) 
+
+        
+    
+
+# def get_highest_word_score(word_list):
+#     calculate_score = get_highest_scoring_words(word_list)
+#     score = calculate_score[1] 
+#     winning_words = calculate_score[0]
+#     shortest_word = calculate_score[2]
+#     winning_word = ''
+
+#     if len(winning_words) == 1: 
+#         winning_word = winning_words[0]
+  
+#     for word in winning_words: 
+#         if len(word) == 10: 
+#             winning_word = word
+#             break
+#         else: 
+#             winning_word = shortest_word 
+    
+#     return tuple(winning_word, score)
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
 
 
 
