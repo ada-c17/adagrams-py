5af9b99dc625329c4876ce545b386c2d7b711eb5
xiomara <xrodri98@gmail.com>
2022-03-30 10:06:52 -0400

    updated_wave_3

--- adagrams/game.py
+++ adagrams/game.py
@@ -40,6 +40,14 @@ score_chart = {
     10: ["Q", "Z"],
 }
 
+scores_chart = {
+    "A": 1, "C": 3, "B": 3, "E": 1, "D": 2, "G": 2, 
+    "F": 4, "I": 1, "H": 4, "K": 5, "J": 8, "M": 3, 
+    "L": 1, "O": 1, "N": 1, "Q": 10, "P": 3, "S": 1, 
+    "R": 1, "U": 1, "T": 1, "W": 4, "V": 4, "Y": 4, 
+    "X": 8, "Z": 10
+}
+
 
 # result = random.choice(list(LETTER_POOL))
 # print(result)
@@ -135,8 +143,24 @@ def score_word(word):
     if lenth > 7 and lenth < 11:
         score += 8
 
-    """
 
+
+scores_chart = {"A": 1, "C": 3, "B": 3, "E": 1, "D": 2, "G": 2, 
+         "F": 4, "I": 1, "H": 4, "K": 5, "J": 8, "M": 3, 
+         "L": 1, "O": 1, "N": 1, "Q": 10, "P": 3, "S": 1, 
+         "R": 1, "U": 1, "T": 1, "W": 4, "V": 4, "Y": 4, 
+         "X": 8, "Z": 10}
+    """
+    word = word.upper()
+    score = 0
+    for letter in word:
+        score += scores_chart[letter]
+    if len(word) >= 7 and len(word)< 11:
+        # if len(word) == 7 or len(word) == 8 or len(word) == 9 or len(word) == 10:
+        # if len(word) in [7, 8, 9, 10]:
+        score +=  8
+
+    return score 
     
 
     
