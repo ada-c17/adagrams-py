f67ca9897e8671f6aec7133219a50f3306a9a1a5
Ruge Li <rugeli0605@gmail.com>
2022-03-30 10:15:44 -0700

    project completed

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,5 +1,6 @@
 import random
 import copy
+from webbrowser import get
 
 LETTER_POOL = {
     'A': 9, 
@@ -30,6 +31,35 @@ LETTER_POOL = {
     'Z': 1
 }
 
+SCORE_CHART = {
+    'A': 1, 
+    'B': 3, 
+    'C': 3, 
+    'D': 2, 
+    'E': 1, 
+    'F': 4, 
+    'G': 2, 
+    'H': 4, 
+    'I': 1, 
+    'J': 8, 
+    'K': 5, 
+    'L': 1, 
+    'M': 3, 
+    'N': 1, 
+    'O': 1, 
+    'P': 3, 
+    'Q': 10, 
+    'R': 1, 
+    'S': 1, 
+    'T': 1, 
+    'U': 1, 
+    'V': 4, 
+    'W': 4, 
+    'X': 8, 
+    'Y': 4, 
+    'Z': 10
+}
+
 def draw_letters():
     '''This function returns 10 random letter tiles. Frequency of letters determined by LETTER_POOL'''
     letters = []
@@ -40,9 +70,8 @@ def draw_letters():
             continue
         else:
             letters.append(random_letter)
-            letter_pool_copy[random_letter] = letter_pool_copy[random_letter]-1
+            letter_pool_copy[random_letter] -= 1
     return letters
-
 
     '''PSUEDOCODE:
     1) Make deep copy of LETTER_POOL
@@ -70,9 +99,51 @@ def uses_available_letters(word, letter_bank):
         return False
 
 def score_word(word):
-    pass
+    '''Calculates total value of word based on letter values in SCORE_CHART'''
+    word_score = 0
+    word = word.upper()
+    for letter in word:
+        word_score += SCORE_CHART[letter]
+    if len(word) in range(7,11):
+        word_score += 8
+    return word_score
 
 def get_highest_word_score(word_list):
-    pass
+    '''Returns a tuple: ("Word", word_score) which represents the winning word.
+       Tie-breakers: Words over 10 letters or shortest word wins'''
+    
+    scores = []
+    best_words = []
+    winning_word = []
 
-print(draw_letters())
+    # Find highest score (int)
+    for word in word_list:
+        scores.append(score_word(word))
+    highest_score = max(scores)
+
+    # Create a list of all the words that equal the highest score
+    for word in word_list:
+        if score_word(word) == highest_score:
+            best_words.append(word)
+
+    # If only one word equals highest score
+    if len(best_words) == 1:
+        winning_word.append(best_words[0])
+        winning_word.append(score_word(best_words[0]))
+        return tuple(winning_word)
+    
+    # Tie-Breaker for multple words with highest score
+    good_word = best_words[0]
+    for word in best_words:
+        if len(word) == 10:
+            good_word = word
+            winning_word.append(good_word)
+            winning_word.append(score_word(good_word))
+            return tuple(winning_word)
+        elif len(word) < len(good_word):
+            good_word = word
+    winning_word.append(good_word)
+    winning_word.append(score_word(good_word))
+    return tuple(winning_word)
+
+
