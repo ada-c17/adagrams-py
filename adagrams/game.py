df7a1752f8b94c10ec266cb165ace9c52306af7c
Lin Liu <lin_6066@yahoo.com>
2022-03-29 22:19:05 -0700

    add comments for refactoring

--- adagrams/game.py
+++ adagrams/game.py
@@ -42,8 +42,8 @@ SCORE_CHART = {
 
 def draw_letters():
 
-    letter_pool_copy = copy.deepcopy(LETTER_POOL)
-    drawn_letters = []
+    letter_pool_copy = copy.deepcopy(LETTER_POOL)   # shallow copy should work in this case? it's faster than deep copy, deep copy complexity is varied
+    drawn_letters = []                              
 
     for i in range(10):
         letter = random.choice(list(letter_pool_copy.keys()))
@@ -55,21 +55,20 @@ def draw_letters():
     return drawn_letters
 
 def uses_available_letters(word, letter_bank):
-    letter_bank_copy = copy.deepcopy(letter_bank)
-    for char in word:
-        char = char.capitalize()
-        if char in letter_bank_copy:
-            letter_bank_copy.remove(char)
-        else:
-            return False
+    letter_bank_copy = copy.deepcopy(letter_bank)   # 2nd thought: we might use a dictionary to store the letter data? key:value --> letter:freq
+    for char in word:                               # checking an item in dictionary is O(1), checking an item in list is O(n)
+        char = char.capitalize()                    # the good thing is, we only have 10 letters so the space complextiy will be O(1) no matter what
+        if char not in letter_bank_copy:        
+            return False            # Switch the order of the return and remove
+        letter_bank_copy.remove(char)               # list.remove method time complexity is O(n), update the value in dict is O(1) in the most cases
     return True
 
 def score_word(word):
     score = 0
     word = word.upper()
-    for letter in word:
-        for char, value in SCORE_CHART.items():
-            if letter in char:
+    for letter in word:                            # still thinking might use update the SCORE_CHART ----> single_letter:score
+        for char, value in SCORE_CHART.items():    # it reduce the time complexity from n^2 to n, and it won't take too many space because there're
+            if letter in char:                     # 26 letters at the most, how do you think :)
                 score += value
 
     if len(word) >= 7:
@@ -83,9 +82,9 @@ def get_highest_word_score(word_list):
     words_score = {}
     words_max_value = []
 
-    for word in word_list:
-        words_score[word] = score_word(word)
-    
+    for word in word_list:                          # line 85 to 88 can be combined. Within one iteration, we can get the score and max_value. 
+        words_score[word] = score_word(word)        # and we can get length as well. but I couldn't think about an effiency data structure to store
+                                                    # that many information at once without using nested array :(
     max_value = max(words_score.values())
 
     for word, score in words_score.items():
@@ -98,3 +97,25 @@ def get_highest_word_score(word_list):
             if len(word) == 10:
                 return word, max_value    
     return words_max_value[0], max_value
+
+
+def wave4(word_list):
+    word_list = sorted(word_list, key = len)
+    max_value = 0
+    words_max_value = []
+    words_score = {}
+
+    for word in word_list:
+        cur_score = score_word(word)
+        words_score[word] = cur_score
+        max_value = max(max_value, cur_score)
+    
+    for i in range(len(words_score)-1, -1, -1):
+        cur_word = words_score[i]
+        if len(cur_word) != 10 or i == 0:
+            return words_score[0], max_value
+        elif len(words_score[i-1]) != 10:
+            return words_score[i], max_value
+
+
+print(wave4(["XXX", "XXXX", "XX", "X"]))
