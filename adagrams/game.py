90f8190b9b1c1815aa41e15ee81e0aae1b27efe1
Emily Schell <lauraemilyschell@gmail.com>
2022-03-30 09:52:02 -0400

    reconcile file differences to have correct versions of functions

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,7 +1,8 @@
 import random
 '''
-We are importing the random module so we can access its .choice() method to randomly select letters
+Importing random module so we can use its .choice() method to randomly select letters from list
 '''
+
 
 LETTER_POOL = {
     'A': 9,
@@ -31,6 +32,7 @@ LETTER_POOL = {
     'Y': 2,
     'Z': 1
 }
+
 # -----test_wave_01-----------
 
 
@@ -60,50 +62,69 @@ def draw_letters():
             num_letters += 1
     return new_letter_list
 
+# -------test_wave_02---------------
+
 
 def uses_available_letters(word, letter_bank):
-    letters_valid = True
-    letter_bank_copy = []
-    for letter in letter_bank:
-        letter_bank_copy.append(letter.upper())
+    word = word.upper()
+
     for letter in word:
-        letter = letter.upper()
-        if letter in letter_bank_copy:
-            letter_bank_copy.remove(letter)
+        keep_checking = True
+        if letter in letter_bank:
+            letter_count = word.count(letter)
+            letter_list_count = letter_bank.count(letter)
+            if letter_count <= letter_list_count:
+                keep_checking
+            else:
+                keep_checking = False
+                break
         else:
-            letters_valid = False
-    return letters_valid
+            return False
+    return keep_checking
+
+# -----test_wave_03-----------
+
+
+score_dict = {
+    'A': 1,
+    'B': 3,
+    'C': 3,
+    'D': 2,
+    'E': 1,
+    'F': 4,
+    'G': 2,
+    'H': 4,
+    'I': 1,
+    'J': 8,
+    'K': 5,
+    'L': 1,
+    'M': 3,
+    'N': 1,
+    'O': 1,
+    'P': 3,
+    'Q': 10,
+    'R': 1,
+    'S': 1,
+    'T': 1,
+    'U': 1,
+    'V': 4,
+    'W': 4,
+    'X': 8,
+    'Y': 4,
+    'Z': 10
+}
 
 
 def score_word(word):
+    score_point = 0
+    word = word.upper()
+    for letter in word:
+        score_point += score_dict[letter]
+    if len(word) >= 7:
+        score_point += 8
+    return score_point
 
-    points_dict = {}
-    for letter in ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"]:
-        points_dict[letter] = 1
-    for letter in ["D", "G"]:
-        points_dict[letter] = 2
-    for letter in ["B", "C", "M", "P"]:
-        points_dict[letter] = 3
-    for letter in ["F", "H", "V", "W", "Y"]:
-        points_dict[letter] = 4
-    for letter in ["K"]:
-        points_dict[letter] = 5
-    for letter in ["J", "X"]:
-        points_dict[letter] = 8
-    for letter in ["Q", "Z"]:
-        points_dict[letter] = 10
-
-    score = 0
-
-    for char in word.upper():
-        if char not in points_dict:
-            continue
-        else:
-            score += points_dict[char]
-    if len(word) in range(7, 11):
-        score += 8
-
-    return score
+# ------test_wave04-------
 
 
 def get_highest_word_score(word_list):
