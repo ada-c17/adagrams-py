27d1e7c0f08700e80f9b2b751abccac809d6906c
Emily Saeli <emilysaeli@gmail.com>
2022-03-30 19:51:55 -0700

    Passing all but one test

--- adagrams/game.py
+++ adagrams/game.py
@@ -82,12 +82,23 @@ def get_highest_word_score(word_list):
     highest_score = 0
     highest_word = ()
     for word in word_list:
-        if score_word(word) > highest_score:
+        if score_word(word) == highest_score:
+            if len(word) == 10:
+                if len(highest_word) == 10:
+                    continue
+                else:
+                    highest_word = word
+            elif len(word) < len(highest_word):
+                highest_word = word
+            elif len(word) == len(highest_word):
+                continue
+            elif len(word) > len(highest_word):
+                continue
+            
+        elif score_word(word) > highest_score:
             highest_score = score_word(word)
-            highest_word = word
-        else:
-            continue
-    return (highest_word, highest_score)
+            highest_word = word  
+    return (highest_word, highest_score) 
     
 
 
