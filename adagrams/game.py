526137be8f98682222b5d7afbde64539e8c9fd6e
Esther Annorzie <estherannorzie@gmail.com>
2022-03-31 15:08:39 -0400

    added docstrings to functions

--- adagrams/game.py
+++ adagrams/game.py
@@ -42,17 +42,9 @@ SCORE_CHART = {
 
 def draw_letters():
     '''
-    1) import the random module and make LETTER_POOL a global variable
-    2) how should we iterate through the LETTER_POOL?
-        a) make each letter-value pair a dictionary as an element in a tuple/list?
-        B) put values of the letters in a list (Like nine A elements, two B elements, etc.)
-    3) use a method from the random module to shuffle the list
-    4) return the first 10 elements in the shuffled list
+    Creates a new randomized list of letters from LETTER_POOL.
+    Returns: A randomized list of ten letters.
     '''
-    # letters = []
-    # for letter, number in LETTER_POOL.items():
-    #     for i in range(number):
-    #         letters.append(letter)
 
     letters = [letter for letter, letter_frequency in LETTER_POOL.items() for number in range(letter_frequency)]
 
@@ -62,83 +54,68 @@ def draw_letters():
 
 def uses_available_letters(word, letter_bank):
     '''
-    1) Iterate over each letter in word
-    2) If the letter is not in letter_bank or the frequency of 
-    the letter in word is greater than the frequency of the 
-    letter in letter_bank...
-    3) is_valid becomes False
-    4) Break the loop
-    5) return is_valid
-    6) For the last test: the letters in word can be lower or upper case.
-    7) The letter can only be used as frequent as it is in letter_bank.
+    Checks that each letter is listed in the dictionary as >= 
+    as used in the word.
+
+    Parameters: string, dict (word and letter frequency)
+    Returns a Boolean value representing the validity of the input.
     '''
-    # letter_bank_uppercase = list(("".join(letter_bank)).upper())
-    # letter_bank_lowercase = list(("".join(letter_bank)).lower())
-    # letter_bank_diff_cases = tuple(letter_bank_uppercase + letter_bank_lowercase)
-    # print(letter_bank_diff_cases)
 
     word_uppercase = word.upper()
     
-    is_valid = True
     for letter in word_uppercase:
         if letter not in letter_bank or (word_uppercase.count(letter) > letter_bank.count(letter)):
-            is_valid = False
-            break
+            return False
         
-    return is_valid
+    return True
 
 
 def score_word(word):
     '''
-    Create a SCORE_CHART dictionary with letters: point values where letters is a tuple of letters and values are integers.
-    Initialize score integer.
-    If length of word between 7 and 10, add 8 to score.
-    iterate over string
-        iterate over the dictionary to get the point value
-            add to score
-    return score
+    Score is given to each letter based on the score chart dictionary.
+    If the word is between 7 and 10, add 8 points to score.
+
+    Parameters: A string of the word.
+
+    Returns the score of the word.
     '''
     score = 0
     word_uppercase = word.upper()
     if len(word_uppercase) >= 7 and len(word_uppercase) <= 10: 
         score += 8
     for letter in word_uppercase:
-        for k in SCORE_CHART:
-            if letter in k:
-                score += SCORE_CHART[k]
+        for letter_freq in SCORE_CHART:
+            if letter in letter_freq:
+                score += SCORE_CHART[letter_freq]
     return score
 
 
 def get_index_tie_break(word_list):
     '''
-    Finds the winner to break a tie with rules: 10 letters > fewest letters > first word 
-    Not sure why, but range(len(word_list)-1) didn't iterate over the loop properly. 
+    Checks if the length of the word is 10, then checks for the first word with the fewest letters
+    Otherwise, returns the first word.
+
+    Parameter: List of highest scoring, tied words.
+
+    Return the index of the winning word.
     '''
     highest_index = 0
-    for i in range(len(word_list)): # I would skip the first item but we need to check if it is the shortest or 10 letters long. 
+    for i in range(len(word_list)):
         if len(word_list[i]) == 10: 
-            # highest_index = i # maybe return i at this point? otherwise, highest_rated will get overwritten by shorter words later.
             return i
-        elif len(word_list[i]) < len(word_list[highest_index]): # will this work if the first word is 10 letters long?
+        elif len(word_list[i]) < len(word_list[highest_index]):
             highest_index = i
     return highest_index
 
 
 def get_highest_word_score(word_list):
     '''
-    ties: 
-        10 letters > fewest letters > first word 
-    1) initialize an empty list variable for the highested_scoring_words
-    2) initialize highested_score variable
-    3) iterate over the word in word_list
-    4) call score_word on each of the words
-    5) store all of the highested scoring words into call score_word on each of the words
-    6) if score > highested_score, overwrite the list with highest_   score_words, overwrite the highest_score with the score
-    7) elif score == highested_score, add it to the highested_scoring_words
-    8) if length of highested_scoring_words > 1, tie break function?
-    9) Tie-break should return the index of the highested scoring word
-    10) the a tuple of highest_scoring_words the first element and
-    the score
+    Calculate high score for each word using score_word and calculate the winner if there is a 
+    tie using get_index_tie_break(). 
+
+    Parameter: A list of words.
+
+    Returns a tuple of the highest scoring word and its high score.
     '''
     highest_scoring_words = []
     high_score = 0
