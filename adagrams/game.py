d6d0bef342fe4efedc3520eee50b3e86c129eb85
San <san.robinxin@gmail.com>
2022-03-31 16:17:38 -0500

    alomost final

--- adagrams/game.py
+++ adagrams/game.py
@@ -35,6 +35,33 @@ LETTER_POOL = {
 }
 
 
+POINT_SCALE = {'A': 1, 
+    'B': 3, 
+    'C': 3, 
+    'D': 2, 
+    'E': 1, 
+    'F': 4, 
+    'G': 2, 
+    'H': 4, 
+    'I': 1, 
+    'J': 8, 
+    'K': 5, 
+    'L': 1, 
+    'M': 3, 
+    'N': 1, 
+    'O': 1, 
+    'P': 3, 
+    'Q': 10, 
+    'R': 1, 
+    'S': 1, 
+    'T': 1, 
+    'U': 1, 
+    'V': 4, 
+    'W': 4, 
+    'X': 8, 
+    'Y': 4, 
+    'Z': 10}
+
 def draw_letters():
 
     letters = []
@@ -60,32 +87,14 @@ def uses_available_letters(word, letter_bank):
 
 
 def score_word(word):
-    score = 0
-    one_point = ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T']
-    two_points = ['D', 'G']
-    three_points = ['B', 'C', 'M', 'P']
-    four_points = ['F', 'H', 'V', 'W', 'Y']
-    five_points = ['K']
-    eight_points = ['J', 'X']
-    ten_points = ['Q', 'Z']
-    for letter in word.upper():
-        if letter in one_point:
-            score += 1
-        elif letter in two_points:
-            score += 2
-        elif letter in three_points:
-            score += 3
-        elif letter in four_points:
-            score += 4
-        elif letter in five_points:
-            score += 5
-        elif letter in eight_points:
-            score += 8
-        elif letter in ten_points:
-            score += 10
+
+    total = 0
+    for letter in word:
+        total += POINT_SCALE[letter.upper()]
     if len(word) >= 7:
-        score += 8
-    return score 
+        total += 8
+
+    return total
 
 
 def get_highest_word_score(word_list):
@@ -93,7 +102,6 @@ def get_highest_word_score(word_list):
     for word in word_list:
         word_scores[word] = score_word(word)
 
-    #highest_word = max(word_scores, key=word_scores.get)
     highest_score = max(word_scores.values())
     
     top_words = []
@@ -103,55 +111,13 @@ def get_highest_word_score(word_list):
 
     shortest_word = min(top_words, key=len)  
     sorted_scores = sorted(word_scores, key=len, reverse=True)
-    #print(sorted_scores)   
+
     
     for word in sorted_scores:
         if word in top_words:
             if len(word) == 10:
-                # print(word)
-                # print(word_scores[word])
                 return tuple((word, word_scores[word]))
             elif word == shortest_word:
-                # print(word)
-                # print(word_scores[word])
                 return tuple((word, word_scores[word]))
 
 
-
-
-
-
-
-
-
-#     letter_pool = [
-#         {'A': 9}, 
-#     {'B': 2}, 
-#     {'C': 2}, 
-#     {'D': 4}, 
-#     {'E': 12}, 
-#     {'F': 2}, 
-#     {'G': 3}, 
-#     {'H': 2}, 
-#     {'I': 9}, 
-#     {'J': 1}, 
-#     {'K': 1}, 
-#     {'L': 4}, 
-#     {'M': 2}, 
-#     {'N': 6}, 
-#     {'O': 8}, 
-#     {'P': 2}, 
-#     {'Q': 1}, 
-#     {'R': 6}, 
-#     {'S': 4}, 
-#     {'T': 6}, 
-#     {'U': 4}, 
-#     {'V': 2}, 
-#     {'W': 2}, 
-#     {'X': 1}, 
-#     {'Y': 2}, 
-#     {'Z': 1}
-# ]
-
-# LETTER = list('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
-    # COUNT = [9,2,2,4,12,2,3,2,9,1,1,4,2,6,8,2,1,6,4,6,4,2,2,1,2,1]
