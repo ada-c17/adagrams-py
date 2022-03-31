d74ef2b471945b34c0e8e90476ed5b4f6c09b424
Kassidy Buslach <kjbuslach@gmail.com>
2022-03-31 07:51:51 -0700

    minor changes

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,38 +1,5 @@
 import random
 
-'''
-Your first task is to build a hand of 10 letters for the user. 
-To do so, implement the function `draw_letters` in `game.py`. 
-This method should have the following properties:
-
-- No parameters
-- Returns an array of ten strings
-  - Each string should contain exactly one letter
-  - These represent the hand of letters that the player has drawn
-- The letters should be randomly drawn from a pool of letters
-  - This letter pool should reflect the distribution of letters as described in the table below
-  - There are only 2 available `C` letters, so `draw_letters` cannot ever return more than 2 `C`s
-  - Since there are 12 `E`s but only 1 `Z`, it should be 12 times as likely for the 
-  user to draw an `E` as a `Z`
-- Invoking this function should **not** change the pool of letters
-  - Imagine that the user returns their hand to the pool before drawing new letters
-
-| Letter : Qty. | Letter : Qty. |
-|:------:|:-----:|
-| A : 9  | N : 6 |
-| B : 2  | O : 8 |
-| C : 2  | P : 2 |
-| D : 4  | Q : 1 |
-| E : 12 | R : 6 |
-| F : 2  | S : 4 |
-| G : 3  | T : 6 |
-| H : 2  | U : 4 |
-| I : 9  | V : 2 |
-| J : 1  | W : 2 |
-| K : 1  | X : 1 |
-| L : 4  | Y : 2 |
-| M : 2  | Z : 1 |
-'''
 
 LETTER_QUANTITY_DICT = {
     'A' : 9, 'N' : 6 ,
@@ -77,22 +44,29 @@ def uses_available_letters(word, letter_bank):
 # If letter.count in word <= letter count in list Return True,
 #  If not correct quantity return False
     
-    if not is_in_list(word,letter_bank):
+    if not check_letters_intersecting(word,letter_bank):
         return False
-    elif is_in_list(word,letter_bank):
+    elif check_letters_intersecting(word,letter_bank):
         for letter in word.upper(): 
             if word.count(letter) <= letter_bank.count(letter):
                 return True
             else: 
                 return False
     
-def is_in_list(word, letter_bank):
+
+    # Function checks if all letters in word are in letter bank
+def check_letters_intersecting(word, letter_bank):
+    # makes word into a set for comparison
     word_set = set()
+    # guard clause
     if not word.upper(): 
         return False
+        # for each letter in word add to word set
     for letter in word.upper(): 
         word_set.add(letter)
+        # compares set with intersection for overlapping letters
     result = word_set & set(letter_bank)
+    # if all the same letters in both sets they will have equal lengths
     if len(result) == len(set(word)):
         return True
     else: 
@@ -150,24 +124,19 @@ def get_highest_word_score(word_list):
 
     word_scores = {}
     highest_score_list = []
-    winning_list = []
     # Goes thru each word and adds word as key and score as value to dictionary
     for word in word_list: 
         word_scores[word] = score_word(word)
-    print(word_scores)
 # evaluates dictionary for highest score - record integer
     highest = max(word_scores.values())
     # goes through every every key, accesses value--> when value is the same as the high score, add to list 
     for word, score in word_scores.items():
         if score == highest:
             highest_score_list.append(word)
-# if no elements in list return none
-    if not highest_score_list:
-        return None
     # when the length of the list is 1, return the tuple of the word and score
-    elif len(highest_score_list) == 1:
-        the_list = [highest_score_list[0], score_word(highest_score_list[0])]
-        return tuple(the_list)
+    if len(highest_score_list) == 1:
+        highest_score_list.append(score_word(highest_score_list[0]))
+        return tuple(highest_score_list)
         # when the length of the list is greater than one, meaning ties
     elif len(highest_score_list) > 1:
         shortest = min(highest_score_list, key=len)
@@ -178,11 +147,7 @@ def get_highest_word_score(word_list):
             for item in highest_score_list: 
                 if len(item) == len(shortest):
                         return highest_score(item)
-            return highest_score(item)
-            # if item != shortest and len(item) != 10:
-            #     winning_list.append(highest_score_list[0])
-            #     winning_list.append(score_word(highest_score_list[0]))
-            #     return tuple(winning_list) 
+            return highest_score(item) 
 
             
 def highest_score(word):
