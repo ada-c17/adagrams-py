92f74e7129cc7779b563fe386669079ab5af01df
Amber <ambertanaka@yahoo.com>
2022-03-31 16:43:24 -0700

    passing wave 3 now

--- adagrams/game.py
+++ adagrams/game.py
@@ -28,7 +28,34 @@ LETTER_POOL = {
     'Y': 2, 
     'Z': 1
 }
-
+LETTER_POINTS = {
+    'A': 1, 
+    'B': 3, 
+    'C': 3, 
+    'D': 2, 
+    'E': 1, 
+    'F': 4, 
+    'G': 2, 
+    'H': 4, 
+    'I': 1, 
+    'J': 8, 
+    'K': 5, 
+    'L': 1, 
+    'M': 3, 
+    'N': 1, 
+    'O': 1, 
+    'P': 3, 
+    'Q': 10, 
+    'R': 1, 
+    'S': 1, 
+    'T': 1, 
+    'U': 1, 
+    'V': 4, 
+    'W': 4, 
+    'X': 8, 
+    'Y': 4, 
+    'Z': 10
+}
 ##----------- WAVE 1 ----------- ##
 def draw_letters():
     letter_pool = copy.deepcopy(LETTER_POOL)
@@ -52,9 +79,17 @@ def uses_available_letters(word, letter_bank):
             letter_bank_copy.remove(letter)
     return True
 
-
+##----------- WAVE 2 ----------- ##
 def score_word(word):
-    pass
+    points = 0
+    cased_word = word.upper()
+    if len(cased_word) >= 7:
+        points += 8
+    for letter in cased_word:
+        points += LETTER_POINTS[letter]
+    
+    return points
+
 
 def get_highest_word_score(word_list):
     pass
